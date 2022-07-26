e4c4a7aa8d9984c369dc24683b2ad03824cab50a
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-26 09:55:47 +0530

    Time: 201 ms (25.67%), Space: 68.3 MB (6.80%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -5,7 +5,7 @@ public:
         int dp[n];//dp[i] max subarray sum that includes a[i]
         dp[0]=a[0];int ans=a[0];
         for(int i=1;i<n;i++){
-            if(a[i]>dp[i-1] && dp[i-1]<=0){dp[i]=a[i];}
+            if(dp[i-1]<=0){dp[i]=a[i];}//carrying sum which is negative is of no use
             else{dp[i]=dp[i-1]+a[i];}
            ans=max(ans,dp[i]);
         }
