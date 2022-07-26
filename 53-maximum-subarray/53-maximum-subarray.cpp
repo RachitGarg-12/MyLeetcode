8857f7898ae3ffdaaea7342b93bddd81506fa120
RachitGarg-12 <87380094+RachitGarg-12@users.noreply.github.com>
2022-07-26 09:32:03 +0530

    Time: 125 ms (86.63%), Space: 68 MB (8.07%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -4,8 +4,7 @@ public:
         int n=a.size();
         int dp[n];dp[0]=a[0];int ans=a[0];
         for(int i=1;i<n;i++){
-            if(a[i]>dp[i-1]){if(dp[i-1]<=0){dp[i]=a[i];}
-                             else{dp[i]=dp[i-1]+a[i];}}
+            if(a[i]>dp[i-1] && dp[i-1]<=0){dp[i]=a[i];}
             else{dp[i]=dp[i-1]+a[i];}
            ans=max(ans,dp[i]);
         }
