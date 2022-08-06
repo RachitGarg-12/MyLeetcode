0a27603d08538c8bd0bc1dd9067d9c9bf1c613bb
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-08-06 10:33:09 +0530

    Time: 1266 ms (5.01%), Space: 327.8 MB (5.41%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,20 +1,33 @@
 class MyHashSet {
 public:
-    unordered_map<int,int> m;
+    vector<list<int>> m;
+    int len;
     MyHashSet() {
-        
+        len=1e6+1;m.resize(len);
     }
-    
+    int hash(int key){
+        return key%len;
+    }
+    list<int> :: iterator search(int key){
+        int i=hash(key);
+        return find(m[i].begin(),m[i].end(),key);  //if not present it will give m[i].end()
+    }
     void add(int key) {
-        m[key]=1;
+       if(contains(key)){return;}
+        int i=hash(key);
+        m[i].push_back(key);
     }
     
     void remove(int key) {
-        m[key]=0;
+        if(contains(key)){
+        int i=hash(key);
+        m[i].erase(search(key));}
     }
     
     bool contains(int key) {
-        return m[key];
+        int i=hash(key);
+        if(search(key)!=m[i].end()){return true;}
+        else{return false;}
     }
 };
 
