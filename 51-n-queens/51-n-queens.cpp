13f425768a70a7296367c18a1d4cbba75002d421
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-08-08 09:35:10 +0530

    Time: 8 ms (71.03%), Space: 11.8 MB (22.56%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    bool safe(int col,int row,vector<string> board,int n){
-        int tempro=row,tempco=col;
-        while(row>=0 && col>=0){
-            if(board[row][col]=='Q') return false;
-            col--;row--;
-        }
-        row=tempro,col=tempco;
-        while(row<n && col>=0){
-            if(board[row][col]=='Q') return false;
-            col--;row++;
-        }
-         row=tempro,col=tempco;
-        while(col--){
-            if(board[row][col]=='Q') return false;
-        }
-        return true;
-    }
-    void solve(vector<vector<string>> &ans,int n,vector<string> board,int col){
+
+    void solve(vector<vector<string>> &ans,int n,vector<string> board,int col,vector<bool> &rowdone,vector<bool> &up,vector<bool> &down){
         if(col==n){ ans.push_back(board);return;}
         
         for(int i=0;i<n;i++){
-            if(safe(col,i,board,n)){
-                board[i][col]='Q';
-                solve(ans,n,board,col+1);
-                board[i][col]='.';
-            }
+           if(rowdone[i]==true) continue;
+           if(down[col+i]==true) continue;
+           if(up[(n-1)-(col-i)]==true) continue;
+            rowdone[i]=true;down[col+i]=true;up[(n-1)-(col-i)]=true;
+            board[i][col]='Q';
+            solve(ans,n,board,col+1,rowdone,up,down);
+            board[i][col]='.';
+             rowdone[i]=false;down[col+i]=false;up[(n-1)-(col-i)]=false;
         }
     }
     
     vector<vector<string>> solveNQueens(int n) {
+    vector<bool> rowdone(n,false);
+    vector<bool> up((2*n)-1,false);
+    vector<bool> down((2*n)-1,false);
         vector<vector<string>> ans;
         string s(n,'.');vector<string> board(n);
         for(int i=0;i<n;i++){board[i]=s;}
-        solve(ans,n,board,0);
+        solve(ans,n,board,0,rowdone,up,down);
         return ans;
     }
 };
