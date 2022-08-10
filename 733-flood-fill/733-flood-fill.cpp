13ed040b837b9518b0e0cb6820aad41a8952e889
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-08-10 17:54:28 +0530

    Time: 20 ms (28.16%), Space: 14.2 MB (25.31%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    void bfs(int samecolor,int i,int j,vector<vector<int>>& grid,vector<vector<int>> &vis,int n,int m,int color){
-        vis[i][j]=1;grid[i][j]=color;
-        queue<pair<int,int>> q;
-        q.push({i,j});
-        while(!q.empty()){
-            pair<int,int> p=q.front();q.pop();
-            int r=p.first,c=p.second;
+    void dfs(int samecolor,int r,int c,vector<vector<int>>& grid,vector<vector<int>> &vis,int n,int m,int color){
+        vis[r][c]=1;grid[r][c]=color;
             if(r>0){
-                if(grid[r-1][c]==samecolor && vis[r-1][c]==0){vis[r-1][c]=1;grid[r-1][c]=color;q.push({r-1,c});}
+                if(grid[r-1][c]==samecolor && vis[r-1][c]==0){dfs(samecolor,r-1,c,grid,vis,n,m,color);}
             }
             if(r<n-1){
-                if(grid[r+1][c]==samecolor && vis[r+1][c]==0){vis[r+1][c]=1;grid[r+1][c]=color;q.push({r+1,c});}
+                if(grid[r+1][c]==samecolor && vis[r+1][c]==0){dfs(samecolor,r+1,c,grid,vis,n,m,color);}
             }
             if(c<m-1){
-                if(grid[r][c+1]==samecolor && vis[r][c+1]==0){vis[r][c+1]=1;grid[r][c+1]=color;q.push({r,c+1});}
+                if(grid[r][c+1]==samecolor && vis[r][c+1]==0){dfs(samecolor,r,c+1,grid,vis,n,m,color);}
             }
              if(c>0){
-                if(grid[r][c-1]==samecolor && vis[r][c-1]==0){vis[r][c-1]=1;grid[r][c-1]=color;q.push({r,c-1});}
+                if(grid[r][c-1]==samecolor && vis[r][c-1]==0){dfs(samecolor,r,c-1,grid,vis,n,m,color);}
             }
-            
-        }
-    }
+}
     vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc, int color) {
         int n=grid.size();int m=grid[0].size();
         vector<vector<int>> vis(n,vector<int>(m,0));
         int samecolor=grid[sr][sc];
-        bfs(samecolor,sr,sc,grid,vis,n,m,color);
+        dfs(samecolor,sr,sc,grid,vis,n,m,color);
         return grid;
         
     }
