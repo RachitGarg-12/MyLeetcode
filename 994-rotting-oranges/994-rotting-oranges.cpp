f059a98b42349c62f7a2f7fdd2e8f736bef199f5
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-08-10 22:49:45 +0530

    Time: 4 ms (92.73%), Space: 13.5 MB (19.16%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,12 +1,9 @@
 class Solution {
 public:
     
-     int bfs(vector<vector<int>>& grid,vector<pair<pair<int,int>,int>> &rotten,vector<vector<int>> &vis,int n,int m){
+     int bfs(vector<vector<int>>& grid,queue<pair<pair<int,int>,int>>& q,vector<vector<int>> &vis,int n,int m){
         int ans=0;
-        queue<pair<pair<int,int>,int>> q;
-       for(auto i:rotten){
-               q.push({{i.first.first,i.first.second},i.second});
-       }
+        
         while(!q.empty()){
             pair<pair<int,int>,int> p=q.front();q.pop();
             int r=p.first.first,c=p.first.second,t=p.second;
@@ -32,15 +29,15 @@ public:
     int orangesRotting(vector<vector<int>>& grid) {
         int n=grid.size();int m=grid[0].size();
         vector<vector<int>> vis(n,vector<int>(m,0));
-        vector<pair<pair<int,int>,int>> rotten;bool done=false;
+        queue<pair<pair<int,int>,int>> q;bool done=false;
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==2 && vis[i][j]==0){
-                    done=true;rotten.push_back({{i,j},0});
+                    done=true;q.push({{i,j},0});
                 }
             }
         }
-        int ans=bfs(grid,rotten,vis,n,m);
+        int ans=bfs(grid,q,vis,n,m);
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1) return -1;
