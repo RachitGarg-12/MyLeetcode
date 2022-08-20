be42402dc731079f826a691ce143450d0b8e857c
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-08-20 12:07:52 +0530

    Time: 133 ms (61.05%), Space: 19.7 MB (97.13%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -5,12 +5,12 @@ public:
         vector<vector<int>> ans;
         for(int i=0;i<n-2;i++){
             if(a[i]>0){continue;}
-            if( i>0 && a[i]==a[i-1]){continue;}
+            if( i>0 && a[i]==a[i-1]){continue;} // no duplicate first element repeatede as we avoid same triplet
             int l=i+1,h=n-1;int target=-a[i];
             while(l<h){
                 if(a[l]+a[h]==target){ans.push_back({a[l],a[h],a[i]});
-                while(l<h && a[l]==a[l+1]){l++;}
-                while(l<h && a[h]==a[h-1]){h--;}
+                while(l<h && a[l]==a[l+1]){l++;} // no 2nd element repeated
+                while(l<h && a[h]==a[h-1]){h--;}  // no 3rd element repeated
                 l++;h--;}
                 else if(a[l]+a[h]+a[i]<0){l++;}
                 else{h--;}
