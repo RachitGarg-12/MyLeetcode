5d24c983e351225d305f8f0b4fe5ce316c186402
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-08-20 09:13:38 +0530

    Time: 535 ms (5.12%), Space: 28.2 MB (52.45%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,26 +1,27 @@
  class Solution {
 public:
-    bool predicate(int target, vector<int>& nums,int m, vector<int> &pref){
-        
-        for(int i=0;i<=nums.size()-m;i++){
-            int temp;
-            if(i==0){temp=pref[i+m-1];}
-            else{temp=pref[i+m-1]-pref[i-1];}
-            if(temp>=target){return true;}
+		int minSubArrayLen(int target, vector<int>& nums) {
+
+		int i = 0, j = 0;
+		int sum = 0;
+		int n = nums.size();
+
+		for(auto it:nums) sum += it;
+		if(sum < target) return 0;
+
+		sum = 0;
+		int ans = INT_MAX;
+
+		while(j < n){
+			sum += nums[j];cout<<i<<" "<<j<<endl;
+			while(sum >= target){
+				ans = min(ans, j-i+1);
+				sum -= nums[i];
+				i++;
+			}
+			j++;
+		}
+
+		return ans;  
         }
-        return false;
-    }
-    int minSubArrayLen(int target, vector<int>& nums) {
-        int n=nums.size();
-        vector<int> pref(n);pref[0]=nums[0];
-        for(int i=1;i<n;i++){pref[i]=pref[i-1]+nums[i];}
-        int l=1,r=nums.size(),ans=r;bool pos=false;
-        while(l<=r){
-            int m=l+(r-l)/2;
-            if(predicate(target,nums,m,pref)){pos=true;ans=m;r=m-1;}
-            else{l=m+1;}
-        }
-        if(pos==false){return 0;}
-        return ans;
-    }
 };
