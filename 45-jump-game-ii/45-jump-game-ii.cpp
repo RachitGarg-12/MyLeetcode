4f70e7f7496982faea8efb2fa0484e7b27405957
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-08-20 17:02:31 +0530

    Time: 516 ms (29.81%), Space: 17.2 MB (39.25%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int jump(vector<int>& a) {
-        int n=a.size();vector<int> dp(n+1,0);
+        int n=a.size();vector<int> dp(n,0);
+        // dp[i] min number of steps u need to reach from i to n-1
         dp[n-1]=0;
         for(int i=n-2;i>=0;i--){
             int ans=1e6;
@@ -12,6 +13,7 @@ public:
            }  
          dp[i]=ans;
         }
-       return dp[0]; 
+        for(auto i:dp) cout<<i<<" ";
+       return dp[0]; // we return dp[0] as we want to reach from 0 to n-1 
     }
 };
