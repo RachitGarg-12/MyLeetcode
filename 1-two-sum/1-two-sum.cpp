c85278c9daf9e7702b4cefec849b7187f24f9e03
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-08-20 13:08:15 +0530

    Time: 11 ms (92.35%), Space: 11 MB (34.29%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -5,8 +5,7 @@ public:
         unordered_map<int,int> m;
         for(int i=0;i<n;i++){
             int left=target-a[i];
-            unordered_map<int,int> :: iterator it=m.find(left);
-           if(it!=m.end()){ans.push_back(it->second);ans.push_back(i);break;} 
+           if(m.find(left)!=m.end()){ans.push_back(m[left]);ans.push_back(i);break;} 
              m[a[i]]=i;
         }
         return ans;
