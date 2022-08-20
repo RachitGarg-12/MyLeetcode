38f2c4c9bd87e3eb689943acc64a36e24ec63558
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-08-20 16:55:43 +0530

    Time: 524 ms (29.33%), Space: 17.2 MB (39.25%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int rec(int i,vector<int>& a,int n,vector<int> &dp){
-        if(i>=n-1) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int ans=1e6;
-        for(int j=1;j<=a[i];j++){
-            int temp= 1+ rec(i+j,a,n,dp);
-            ans=min(ans,temp);
-        }
-        
-        return dp[i]=ans;
-    }
     int jump(vector<int>& a) {
-        int n=a.size();vector<int> dp(n,-1);
-        return rec(0,a,n,dp);
+        int n=a.size();vector<int> dp(n+1,0);
+        dp[n-1]=0;
+        for(int i=n-2;i>=0;i--){
+            int ans=1e6;
+          for(int j=1;j<=a[i];j++){
+            if(i+j>=n) break;
+            int temp= 1+ dp[i+j];
+            ans=min(ans,temp);
+           }  
+         dp[i]=ans;
+        }
+       return dp[0]; 
     }
 };
