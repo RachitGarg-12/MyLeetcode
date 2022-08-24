4280097e76c62158f0771979d4a6f37da51d4170
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-08-24 14:48:00 +0530

    Time: 246 ms (13.97%), Space: 5.9 MB (81.92%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
+    void rec(int i,string &s,int n,int k){
+        if(i==k){return;}
+        int in;
+        for(in=n-2;in>=0;in--){
+            if(s[in]<s[in+1]){break;}
+        }
+        
+        for(int j=n-1;j>in;j--){
+            if(s[j]>s[in]){swap(s[j],s[in]);break;}
+        }
+        reverse(s.begin()+in+1,s.end());
+        rec(i+1,s,n,k);
+    }
     string getPermutation(int n, int k) {
         string s;
         for(int i=1;i<=n;i++){ s+=to_string(i);}
-        int i=1;
-        while(i<=k){
-            if(i==k){break;}
-            next_permutation(s.begin(),s.end());i++;
-        }
+        rec(1,s,n,k);
         return s;
     }
 };
