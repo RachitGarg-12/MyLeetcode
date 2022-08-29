39d79e2fba7b9dcee43e8ca71dbd51b43c0e245c
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-08-29 21:46:53 +0530

    Time: 148 ms (31.24%), Space: 10.3 MB (65.19%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    vector<vector<int>> dp; 
-    int rec(int l,int r,vector<int> &a){
+    vector<vector<int>> dp;vector<int> a;
+    int rec(int l,int r){
     //returns the min cost for vutting in l to r;
     //pruning
     //base case
@@ -9,16 +9,16 @@ public:
     if(dp[l][r]!=-1) return dp[l][r];
     int ans=1e9;
     for(int i=l+1;i<r;i++){
-        ans=min(ans,(a[r]-a[l])+rec(l,i,a)+rec(i,r,a));
+        ans=min(ans,(a[r]-a[l])+rec(l,i)+rec(i,r));
     }
    return dp[l][r]=ans;
 }
     int minCost(int n, vector<int>& cuts) {
-    int l=cuts.size();sort(cuts.begin(),cuts.end());
-    dp.resize(l+2,vector<int>(l+2,-1));
-    vector<int> a(l+2);a[0]=0;
+    int l=cuts.size();
+    sort(cuts.begin(),cuts.end());
+    dp.resize(l+2,vector<int>(l+2,-1));a.resize(l+2);a[0]=0;
     for(int i=1;i<=l;i++){a[i]=cuts[i-1];}
         a[l+1]=n;
-        return rec(0,l+1,a);
+        return rec(0,l+1);
     }
 };
