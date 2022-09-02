d448dc7353cfc301d7c6b5ee5b112dd70230e36a
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-09-02 22:23:26 +0530

    Time: 795 ms (9.97%), Space: 96.7 MB (25.35%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int n,m;vector<vector<int>> vis;bool t=true;
+    void dfs(vector<vector<int>>& a, vector<vector<int>>& b,int i,int j){
+        vis[i][j]=1;
+        // cout<<i<<" "<<j<<endl;
+        if(a[i][j]==0){t=false;}
+        if(i>0 && !vis[i-1][j] && b[i-1][j]==1){dfs(a,b,i-1,j);}
+        if(i<n-1 && !vis[i+1][j] && b[i+1][j]==1){dfs(a,b,i+1,j);}
+        if(j<m-1 && !vis[i][j+1] && b[i][j+1]==1){dfs(a,b,i,j+1);}
+        if(j>0 && !vis[i][j-1] && b[i][j-1]==1){dfs(a,b,i,j-1);}
+    
+    }
+    int countSubIslands(vector<vector<int>>& a, vector<vector<int>>& b) {
+        n=a.size();m=a[0].size();vis.resize(n+1,vector<int>(m+1,0));int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(b[i][j]==1 && !vis[i][j]){
+                  t=true; dfs(a,b,i,j);
+                    if(t){ans++;}
+                }
+            }
+        }
+        return ans;
+    }
+};
