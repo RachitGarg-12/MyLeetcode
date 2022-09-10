3076218bd187d9cc0e0e4a4cf87707c1711ad095
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-09-10 08:42:08 +0530

    Time: 3 ms (87.43%), Space: 16 MB (12.47%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -3,7 +3,6 @@ public:
     vector<string> ans;int t;
     void rec(int i,string p,int score){
         if(i>t){ans.push_back(p);return;}
-        if(score<0)return;
         if(score==0){p+="(";rec(i+1,p,score+1);}
         else{
             if(score>=t-i){p+=")";rec(i+1,p,score-1);}
