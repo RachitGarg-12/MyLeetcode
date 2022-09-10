69c5b8db0a55629ed03f211e2d6683da392221a7
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-09-10 08:38:59 +0530

    Time: 3 ms (87.43%), Space: 15.9 MB (15.36%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> ans;int t;
+    void rec(int i,string p,int score){
+        if(i>t){ans.push_back(p);return;}
+        if(score<0)return;
+        if(score==0){p+="(";rec(i+1,p,score+1);}
+        else{
+            if(score>=t-i){p+=")";rec(i+1,p,score-1);}
+            else{
+               p+="(";rec(i+1,p,score+1);p.pop_back();
+               p+=")";rec(i+1,p,score-1);
+            }
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        t=2*n;string s="";
+        rec(1,s,0);
+        return ans;
+    }
+};
