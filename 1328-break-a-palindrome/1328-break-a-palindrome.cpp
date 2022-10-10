f42b5600b11fc98a5363370e27d42f6ffbc6a1c4
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-10-10 09:22:33 +0530

    Time: 3 ms (45.61%), Space: 6.3 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -3,14 +3,9 @@ public:
     string breakPalindrome(string p) {
         int n=p.size();
         if(n==1){return "";}
-        string ans="";
-        bool b=false;
-        for(int i=0;i<n;i++){
-            if(p[i]!='a'){
-                if(n%2 && i==n/2){continue;}
-                p[i]='a';b=true;break;}
+        for(int i=0;i<n/2;i++){
+            if(p[i]!='a'){p[i]='a';return p;}
         }
-        if(b==true){return p;}
         p[n-1]='b';
         return p;
     }
