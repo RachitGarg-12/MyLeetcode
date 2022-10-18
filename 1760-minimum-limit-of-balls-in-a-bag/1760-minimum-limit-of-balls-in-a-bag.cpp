b2d6575429363fa8e8d01d80914e44c0e44c8e83
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-10-18 09:06:04 +0530

    Time: 356 ms (59.91%), Space: 55.9 MB (76.14%) - LeetHub

---
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool pred(int m,vector<int>& a,int mx){
+        long tot=0;int n=a.size();long comb=n+mx;
+        for(int i=0;i<n;i++){
+            if(a[i]>m){
+                tot+=a[i]/m;if(a[i]%m>0){tot++;}
+                if(tot>comb){return false;}
+            }else{tot++;
+                  if(tot>comb){return false;}}
+        }
+        if(tot>comb){return false;}
+        return true;
+    }
+    int minimumSize(vector<int>& a, int mx) {
+        int l=1,r=1,ans;
+        for(auto it:a){r=max(r,it);}
+        ans=r;
+        while(l<=r){
+            long m=l+(r-l)/2;
+            if(pred(m,a,mx)){ans=m;r=m-1;}
+            else{l=m+1;}
+        }
+        return ans;
+    }
+};
