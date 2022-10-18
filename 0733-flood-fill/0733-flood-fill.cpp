bbb19e3496b8bfd7ef026e49e8f2c7a3c06f7d85
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-10-18 14:23:18 +0530

    Time: 7 ms (96.51%), Space: 14.3 MB (26.79%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> vis;int n,m;
+    void dfs(vector<vector<int>>& a, int i, int j, int color){
+        vis[i][j]=1;
+        
+        if(i>0 && !vis[i-1][j] && a[i-1][j]==a[i][j]){dfs(a,i-1,j,color);}
+        if(i<n-1 && !vis[i+1][j] && a[i+1][j]==a[i][j]){dfs(a,i+1,j,color);}
+        if(j>0 && !vis[i][j-1] && a[i][j-1]==a[i][j]){dfs(a,i,j-1,color);}
+        if(j<m-1 && !vis[i][j+1] && a[i][j+1]==a[i][j]){dfs(a,i,j+1,color);}
+        
+        a[i][j]=color;
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& a, int sr, int sc, int color) {
+        n=a.size();m=a[0].size();
+        vis.resize(n,vector<int>(m,0));
+        dfs(a,sr,sc,color);
+        return a;
+    }
+};
+
