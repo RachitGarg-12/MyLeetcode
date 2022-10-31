ad985a8b133b5cbb31b91a149d7db3197eac3a51
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-10-31 22:30:47 +0530

    Time: 2 ms (68.32%), Space: 5.9 MB (84.58%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int digitSquareSum(int n) {
+        int sum = 0, tmp;
+        while (n) {
+            tmp = n % 10;
+            sum += tmp * tmp;
+            n /= 10;
+        }
+        return sum;
+    }
+    
+    bool isHappy(int n) {
+        int slow, fast;
+        slow = fast = n;
+        do {
+            slow = digitSquareSum(slow);
+            fast = digitSquareSum(fast);
+            fast = digitSquareSum(fast);
+            if(fast == 1) return 1;
+        } while(slow != fast);
+         return 0;
+}
+};
