e1e76129f5a33ee64529c7ef25b7a0d402ac6950
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-11-01 17:44:29 +0530

    Time: 13 ms (57.56%), Space: 6.6 MB (70.46%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.cpp
+++ 0043-multiply-strings/0043-multiply-strings.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     string multiply(string s1, string s2) {
-        if(s1=="0" || s2=="0")return "0";
         int n=s1.size(),m=s2.size();vector<int> s(n+m,0);
         for(int i=n-1;i>=0;i--){
             for(int j=m-1;j>=0;j--){
@@ -15,6 +14,7 @@ public:
           if(i==0 && b==true){continue;}
           else{ans+=to_string(i);b=false;}
       }
+    if(b)return "0";
       return ans;
     }
 };
