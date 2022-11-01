4d8fd7b76d21aabc42143169f60565b58abf932f
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-11-01 11:18:47 +0530

    Time: 128 ms (5.01%), Space: 13.2 MB (52.18%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findBall(vector<vector<int>>& a) {
+        int n=a.size(),m=a[0].size();
+        vector<int> ans;
+        for(int c=0;c<m;c++){
+            int j=0;bool b=true;int i=c;
+            while(j<n && j>=0 && i>=0 && i<m){
+                cout<<j<<" "<<i<<endl;
+                if(a[j][i]==1 && i<m-1 && a[j][i+1]==1){j++;i++;}
+                else if(a[j][i]==-1 && i>0 && a[j][i-1]==-1){i--;j++;}
+                else{b=false;break;}
+            }
+            if(b==false){ans.push_back(-1);}
+            else{ans.push_back(i);}
+        }
+        return ans;
+    }
+};
