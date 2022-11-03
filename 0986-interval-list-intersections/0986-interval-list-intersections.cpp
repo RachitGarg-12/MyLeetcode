1cc3c238e09e13ce886776491272d50ab99d0478
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-11-03 17:36:55 +0530

    Time: 75 ms (50.83%), Space: 18.7 MB (76.95%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -5,16 +5,11 @@ public:
         vector<vector<int>> ans;
         int i=0,j=0;
         while(i<n && j<m){
-            // cout<<i<<" "<<j<<endl;
             if(f[i][0]<=s[j][1] && s[j][0]<=f[i][1]){
                 ans.push_back({max(f[i][0],s[j][0]),min(f[i][1],s[j][1])});
-                if(f[i][1]<s[j][1]){i++;}
-                else if(f[i][1]>s[j][1]){j++;}
-                else{i++;j++;}
             }
-            else if(f[i][1]<s[j][0]){i++;}
-            else if(s[j][1]<f[i][0]){j++;}
-            
+            if(f[i][1]<s[j][1]){i++;}
+            else{j++;}     
         }
         return ans;
     }
