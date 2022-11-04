308fe9f4cca100ddeaabe4b1e5a1f4c5a0617b87
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-11-04 09:48:04 +0530

    Time: 85 ms (68.38%), Space: 35.1 MB (5.84%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -3,14 +3,14 @@ public:
     bool pos(int m,vector<int>a,int h){
         int n=a.size();int t=0;
         for(int i=0;i<n;i++){
-            // cout<<t<<" "<<i<<endl;
             t += a[i]%m==0 ? a[i]/m : (a[i]/m)+1;
             if(t>h)return false;
         }
        return true;    
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int l=1,r=1e9;int ans=0;
+        int l=1,r=0;int ans=0;
+        for(auto i:piles){r=max(r,i);}
         while(l<=r){
             int m=l+(r-l)/2;
             if(pos(m,piles,h)){ans=m;r=m-1;}
