5f4367eeea269cb96dac517c2a5bf92ce99d4142
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-12-15 22:46:24 +0530

    Time: 30 ms (69.77%), Space: 10.9 MB (67.87%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -3,24 +3,18 @@ public:
 int n,m;string a,b;
 int dp[1001][1001];
 int rec(int i,int j){
-    //return the lcs of all a[i...n-1] ans b[j...m-1]
-    //pruning 
-    //base case
-    if(i>=n || j>=m){return 0;}
-
-    //save and compute
-    if(dp[i][j]!=-1){return dp[i][j];}
-    //compute
+     if(i<0 || j<0)return 0;
+    if(dp[i][j]!=-1)return dp[i][j];
     int ans=0;
-    ans=max(ans,rec(i+1,j));
-    ans=max(ans,rec(i,j+1));
-    if(a[i]==b[j]){ans=max(ans,1+rec(i+1,j+1));}
-
+    if(a[i]==b[j]){ans=1+rec(i-1,j-1);}
+    ans=max(ans,rec(i,j-1));
+    ans=max(ans,rec(i-1,j));
     return dp[i][j]=ans;
+                   
 }
     int longestCommonSubsequence(string text1, string text2) {
         a=text1,b=text2;n=a.size(),m=b.size();
         memset(dp,-1,sizeof(dp));
-        return rec(0,0);
+        return rec(n-1,m-1);
     }
 };
