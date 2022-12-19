cdf3e84b6fd0f6c95d759047634f4599b8e875c8
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-12-19 15:10:06 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (80.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,13 +3,11 @@ public:
     bool isValid(string st) {
         stack<char> s;
         for(auto i:st){
-            if(i=='('){s.push(i);}
+            if(i=='(' || i=='[' || i=='{'){s.push(i);}
             if(i==')'){if(s.empty() || s.top()!='(')return false;
                       else{s.pop();}}
-            if(i=='['){s.push(i);}
             if(i==']'){if(s.empty() || s.top()!='[')return false;
                       else{s.pop();}}
-            if(i=='{'){s.push(i);}
             if(i=='}'){if(s.empty() || s.top()!='{')return false;
                       else{s.pop();}}
         }
