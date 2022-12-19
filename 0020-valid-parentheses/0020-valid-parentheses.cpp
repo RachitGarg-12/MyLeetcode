47f949bb2f1192ed55547b5942fef9de8894bc54
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-12-19 15:08:41 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (51.30%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    bool isValid(string s) {
-        int a=0,b=0,c=0;
-        vector<int> rec;
-        for(auto i:s){
-            if(i=='('){a++;rec.push_back(1);}
-            if(i==')'){if(a<=0 || rec.back()!=1)return false;
-                      else{a--;rec.pop_back();}}
-            if(i=='['){b++;rec.push_back(2);}
-            if(i==']'){if(b<=0 || rec.back()!=2)return false;
-                      else{b--;rec.pop_back();}}
-            if(i=='{'){c++;rec.push_back(3);}
-            if(i=='}'){if(c<=0 || rec.back()!=3)return false;
-                      else{c--;rec.pop_back();}}
+    bool isValid(string st) {
+        stack<char> s;
+        for(auto i:st){
+            if(i=='('){s.push(i);}
+            if(i==')'){if(s.empty() || s.top()!='(')return false;
+                      else{s.pop();}}
+            if(i=='['){s.push(i);}
+            if(i==']'){if(s.empty() || s.top()!='[')return false;
+                      else{s.pop();}}
+            if(i=='{'){s.push(i);}
+            if(i=='}'){if(s.empty() || s.top()!='{')return false;
+                      else{s.pop();}}
         }
-        if(a==0 && b==0 && c==0)return true;
+        if(s.empty())return true;
         return false;
     }
 };
