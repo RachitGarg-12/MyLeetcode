3c89f1508d6f1d8d2e905e0c6a2e80d8e6ca7f88
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-12-22 01:43:01 +0530

    Time: 1984 ms (5.02%), Space: 404.3 MB (5.05%) - LeetHub

---
+++ 0077-combinations/0077-combinations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    map<vector<int>,int> m;
+    void rec(int cur,vector<int> a,int n,int k){
+        if(k-a.size()>n-cur){return;}
+        if(a.size()==k){m[a]=1;}
+        for(int i=cur+1;i<=n;i++){
+            a.push_back(i);
+            rec(i,a,n,k);
+            a.pop_back();
+            rec(i,a,n,k);
+        } 
+    }
+    vector<vector<int>> combine(int n, int k) {
+        rec(0,{},n,k);
+        vector<vector<int>> ans;
+        for(auto i:m){
+            ans.push_back(i.first);
+        }
+        return ans;
+    }
+};
