241ff124e3383526d8b5355908bce51bb7249366
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-12-22 02:01:00 +0530

    Time: 42 ms (57.72%), Space: 49.1 MB (19.89%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -3,10 +3,10 @@ public:
     vector<vector<int>> ans;
     void rec(int cur,vector<int> a,int n,int k){
         if(k-a.size()>n-cur+1){return;}
-        if(a.size()==k){ans.push_back(a);return;}
         rec(cur+1,a,n,k);
         a.push_back(cur);
-        rec(cur+1,a,n,k);
+        if(a.size()==k){ans.push_back(a);return;}
+        else{rec(cur+1,a,n,k);}
     }
     vector<vector<int>> combine(int n, int k) {
         rec(1,{},n,k); 
