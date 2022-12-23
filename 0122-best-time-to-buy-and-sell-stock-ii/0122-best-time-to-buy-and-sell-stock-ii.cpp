ffbb1758efc2990b18db7ccec8d6249a1e5452f2
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-12-23 21:41:54 +0530

    Time: 4 ms (93.95%), Space: 19.4 MB (5.66%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dp;
+    int rec(int i,int last,bool bech,vector<int>& a){
+        if(i==a.size()){return 0;}
+        if(dp[i]!=-1){return dp[i];}
+        if(a[i]<last || bech==false){rec(i+1,a[i],true,a);}
+        int ans = max(rec(i+1,a[i],false,a)+a[i]-last,rec(i+1,last,true,a));
+        // cout<<last<<" "<<a[i]<<" "<<last-a[i]<<" "<<bech<<endl;
+        return dp[i]=ans;
+    }
+    int maxProfit(vector<int>& a) {
+        int n=a.size();
+        dp.resize(n,-1);dp[0]=0;
+        int ans=rec(1,a[0],true,a);
+        for(auto i:dp){ans=max(ans,i);}
+        return ans;
+    }
+};
