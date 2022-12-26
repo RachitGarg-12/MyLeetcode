0bec3d23f6bc07e15945d99e3e354723b30c2088
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-12-26 14:53:21 +0530

    Time: 75 ms (84.52%), Space: 48.4 MB (79.88%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,17 +2,11 @@ class Solution {
 public:
     bool canJump(vector<int>& a) {
         int n=a.size();
-        vector<int> v;
+        int maxreach=0;
         for(int i=0;i<n;i++){
-            if(a[i]==0 && i!=n-1){v.push_back(i);}
+            if(maxreach<i)return 0;
+            maxreach=max(maxreach,i+a[i]);
         }
-        for(auto i:v){
-            bool temp=false;
-            for(int j=i-1;j>=0;j--){
-                if(a[j]+j>i){temp=true;break;}
-            }
-            if(temp==false)return false;
-        }
-        return true;
+        return 1;
     }
 };
