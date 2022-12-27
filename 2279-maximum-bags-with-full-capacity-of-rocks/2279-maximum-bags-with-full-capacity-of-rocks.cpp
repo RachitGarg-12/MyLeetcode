a5d920cb0ba8c78e4063c0799a77edb4f60c774a
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2022-12-27 09:44:52 +0530

    Time: 206 ms (85.49%), Space: 84.7 MB (91.43%) - LeetHub

--- 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -2,18 +2,16 @@ class Solution {
 public:
     int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
         int n=capacity.size();
-        map<int,int> m;
-        for(int i=0;i<n;i++){m[capacity[i]-rocks[i]]++;}
+        for(int i=0;i<n;i++){
+            rocks[i]=capacity[i]-rocks[i];
+        }
         int ans=0;
-        for(auto i:m){
-            if(additionalRocks>i.first*i.second){
-                additionalRocks -= i.first*i.second;
-                ans+=i.second;
-            }else{
-                ans += additionalRocks/i.first;
-                additionalRocks=0; 
-                break;
-            }
+        sort(rocks.begin(),rocks.end());
+        int cur=0;
+        for(auto i:rocks){
+            cur+=i;
+            if(cur>additionalRocks)break;
+            ans++;
         }
         return ans;
     }
