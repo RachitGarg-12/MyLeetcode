109af9dee3711ecf8241e0b7703bdc895d00acea
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-01 16:19:50 +0530

    Time: 3 ms (97.91%), Space: 13.3 MB (30.66%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+     int bfs(vector<vector<int>>& grid,queue<pair<pair<int,int>,int>>& q,vector<vector<int>> &vis,int n,int m){
+        int ans=0;
+        
+        while(!q.empty()){
+            pair<pair<int,int>,int> p=q.front();q.pop();
+            int r=p.first.first,c=p.first.second,t=p.second;
+            ans=max(ans,t);
+            if(r>0){
+                if(grid[r-1][c]==1 && vis[r-1][c]==0){
+                    vis[r-1][c]=1;q.push({{r-1,c},t+1});grid[r-1][c]=2;}
+            }
+            if(r<n-1){
+                if(grid[r+1][c]==1 && vis[r+1][c]==0){vis[r+1][c]=1;q.push({{r+1,c},t+1});grid[r+1][c]=2;}
+            }
+            if(c<m-1){
+                if(grid[r][c+1]==1 && vis[r][c+1]==0){vis[r][c+1]=1;q.push({{r,c+1},t+1});grid[r][c+1]=2;}
+            }
+             if(c>0){
+                if(grid[r][c-1]==1 && vis[r][c-1]==0){vis[r][c-1]=1;q.push({{r,c-1},t+1});grid[r][c-1]=2;}
+            }
+            
+        }
+         return ans;
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();int m=grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>> q;bool done=false;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2 && vis[i][j]==0){
+                    done=true;q.push({{i,j},0});
+                }
+            }
+        }
+        int ans=bfs(grid,q,vis,n,m);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1) return -1;
+            }}
+      
+        return done?ans:0;
+    }
+};
