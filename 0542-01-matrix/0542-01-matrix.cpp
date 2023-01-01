cbe367c9e0d944213a7899a426a5edcfa1612d16
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-01 20:54:29 +0530

    Time: 566 ms (5.04%), Space: 34.5 MB (26.55%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,66 +1,47 @@
 class Solution {
 public:
- 
-        int di[4]={1,-1,0,0};
-int dj[4]={0,0,1,-1};
+     int n,m;
+     void bfs(vector<vector<int>>& grid,queue<pair<pair<int,int>,int>>& q,vector<vector<int>> &vis,vector<vector<int>> &dis){      
+        while(!q.empty()){
+            pair<pair<int,int>,int> p=q.front();
+            q.pop();
+            int r=p.first.first,c=p.first.second,t=p.second;
+            vis[r][c]=1;
+            dis[r][c]=t;
+            cout<<r<<" "<<c<<" "<<t<<endl;
+            if(r-1>=0 && grid[r-1][c]==1 && vis[r-1][c]==0){
+                    q.push({{r-1,c},t+1});vis[r+-1][c]=1;
+            dis[r-1][c]=t+1;
+            }
+            if(r<n-1 && grid[r+1][c]==1 && vis[r+1][c]==0){                                                   q.push({{r+1,c},t+1});vis[r+1][c]=1;
+            dis[r+1][c]=t+1;
+            }
+            if(c<m-1 && grid[r][c+1]==1 && vis[r][c+1]==0){
+                    q.push({{r,c+1},t+1});vis[r][c+1]=1;
+            dis[r][c+1]=t+1;
+            }
+            if(c-1>=0 && grid[r][c-1]==1 && vis[r][c-1]==0){
+                     q.push({{r,c-1},t+1});vis[r][c-1]=1;
+            dis[r][c-1]=t+1;
+            }
+            
+        }
 
-bool isValid(int row,int col,int m,int n)
-{
-    if(row<0||col<0||row>=m||col>=n)return false;
-    
-    return true;
-}
-vector<vector<int>> updateMatrix(vector<vector<int>>& mat) 
-{
-    int INF=1e7;
-    int m=mat.size();
-    int n=mat[0].size();
-    queue<pair<int,int>>q;
-    
-    //Vector To Store Minimum Distance From (i,j) to 0
-    vector<vector<int>>dist(m,vector<int>(n,INF));
-    // vis[i][j] -> 1 if we already visited (i, j), 0 otherwise
-    vector<vector<int>>vis(m,vector<int>(n,0));
-    
-    //Traverse Over The Whole Matrix And push all (i,j) into queue if mat[i][j]==0
-    for(int i=0;i<m;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            if(mat[i][j]==0)
-            {
-               q.push({i, j});
-               // the shortest distance form any 0 to 0 is 0
-               dist[i][j] = 0;
-               // Also mark the cell as visited
-               vis[i][j] = 1;
+    }
+
+vector<vector<int>> updateMatrix(vector<vector<int>>& grid) {
+        n=grid.size(),m=grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        vector<vector<int>> dis(n,vector<int>(m,-1));
+        queue<pair<pair<int,int>,int>> q;bool done=false;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==0){
+                    q.push({{i,j},0});
+                }
             }
         }
-    }
-    
-    while(!q.empty())
-    {
-        
-         pair<int,int>p=q.front();
-         int i = p.first;
-         int j = p.second;
-         q.pop();
-        
-         for(int k=0;k<4;k++)
-         {
-             // if this Neighbour cell Is not visited and lies inside the matrix
-             if(isValid(i+di[k],j+dj[k],m,n) && !vis[i+di[k]][j+dj[k]])
-             {
-                //Update The Distance of the neighbour cell
-                dist[i+di[k]][j+dj[k]] = min(dist[i+di[k]][j+dj[k]], 1 + dist[i][j]);
-                // mark this neighbour cell visited
-                vis[i+di[k]][j+dj[k]] = 1;
-                // push neighbour cell into the queue to continue the bfs
-                q.push({i+di[k], j+dj[k]});
-             }
-         }
-    }
-    return dist;
-    
+       bfs(grid,q,vis,dis);
+       return dis;
     }
 };
