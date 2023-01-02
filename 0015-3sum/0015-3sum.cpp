0a842359a3a60bbb9d0ef0942e386cc9de643c5b
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-02 18:44:37 +0530

    Time: 1420 ms (10.14%), Space: 183.9 MB (5.73%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
-    vector<vector<int>> threeSum(vector<int>& a) {
-        int n=a.size();sort(a.begin(),a.end());
-        vector<vector<int>> ans;
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        set<vector<int>> ans;
         for(int i=0;i<n-2;i++){
-            if(a[i]>0){continue;}
-            if( i>0 && a[i]==a[i-1]){continue;} // no duplicate first element repeatede as we avoid same triplet
-            int l=i+1,h=n-1;int target=-a[i];
-            while(l<h){
-                if(a[l]+a[h]==target){ans.push_back({a[l],a[h],a[i]});
-                while(l<h && a[l]==a[l+1]){l++;} // no 2nd element repeated
-                while(l<h && a[h]==a[h-1]){h--;}  // no 3rd element repeated
-                l++;h--;}
-                else if(a[l]+a[h]+a[i]<0){l++;}
-                else{h--;}
+            int j=i+1,k=n-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0){ans.insert({nums[i],nums[j],nums[k]});j++;k--;}
+                else if(sum>0){k--;}
+                else{j++;}
             }
         }
-        return ans;
+        vector<vector<int>> res;
+        for(auto i:ans){res.push_back(i);}
+        return res;
     }
 };
