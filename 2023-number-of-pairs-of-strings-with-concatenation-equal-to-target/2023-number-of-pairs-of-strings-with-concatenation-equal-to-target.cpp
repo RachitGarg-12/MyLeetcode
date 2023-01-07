09d02961ad58a4e76cc790d6984aec5319f76a06
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-07 12:18:51 +0530

    Time: 143 ms (39.68%), Space: 67.3 MB (27.29%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -6,10 +6,8 @@ public:
         int ans=0;
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
-                if(nums[i].size()+nums[j].size()==m){
-                    if(nums[i]+nums[j]==target){ans++;}
-                    if(nums[j]+nums[i]==target){ans++;}
-                }
+                if(nums[i]+nums[j]==target){ans++;}
+                if(nums[j]+nums[i]==target){ans++;}
             }
         }
         return ans;
