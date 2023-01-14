03d9f97eb3f6568d6b72b095c8b4f565fd85d63a
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-14 19:32:10 +0530

    Time: 13 ms (82.18%), Space: 10.3 MB (44.35%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,50 +1,44 @@
 class Solution {
 public:
-    vector<int> delrow = {-1,0,1,0};
-    vector<int> delcol = {0,-1,0,1};
+    int n,m;
 
-    void dfs(int row,int col,vector<vector<char>>& board,vector<vector<int>>& vis){
-             vis[row][col]=1;
-              int m = board.size();   // rows
-              int n = board[0].size(); //cols
-             for(int i=0;i<4;i++){
-                 int nrow = row+delrow[i];
-                 int ncol = col+delcol[i];
-                 if(nrow>=0 && ncol >=0 && nrow<m && ncol<n &&
-                 vis[nrow][ncol]!=1 && board[nrow][ncol]=='O'){
-                     dfs(nrow,ncol,board,vis);
-                 }
-             }
+    void dfs(int i,int j,vector<vector<char>>& board,vector<vector<int>> &vis){
+        vis[i][j]=1;
+        if(i-1>=0 && board[i-1][j]=='O' && !vis[i-1][j]){dfs(i-1,j,board,vis);}
+        if(j-1>=0 && board[i][j-1]=='O' && !vis[i][j-1] ){dfs(i,j-1,board,vis);}
+        if(i+1<n && board[i+1][j]=='O' && !vis[i+1][j]){dfs(i+1,j,board,vis);}
+        if(j+1<m && board[i][j+1]=='O' && !vis[i][j+1] ){dfs(i,j+1,board,vis);}
+        
     }
     void solve(vector<vector<char>>& board) {
-         int m = board.size();   // rows
-         int n = board[0].size(); //cols
-         vector<vector<int>> vis(m,vector<int>(n,0));
-         //   Checking on left and right sides
-         for(int j=0;j<n;j++){
-             if(vis[0][j]!=1 && board[0][j]=='O'){
-                 dfs(0,j,board,vis);
-             }
-             if(vis[m-1][j]!=1 && board[m-1][j]=='O'){
-                 dfs(m-1,j,board,vis);
-             }
-         }
-         //Checking on the first and last row Boundary
-         for(int i=0;i<m;i++){
-             if(vis[i][0]!=1 && board[i][0]=='O'){
-                 dfs(i,0,board,vis);
-             }
-             if(vis[i][n-1]!=1 && board[i][n-1]=='O'){
-                 dfs(i,n-1,board,vis);
-             }
-         }
-         //Replacing X with O
-         for(int i=0;i<m;i++){
-             for(int j =0;j<n;j++){
-                   if(vis[i][j]!=1 && board[i][j]=='O'){
-                       board[i][j] = 'X';
-                   }
-             }
-         }
+        n=board.size();
+        m=board[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+                if(!vis[i][0] && board[i][0]=='O'){
+                    dfs(i,0,board,vis);
+                }
+        }
+        for(int j=0;j<m;j++){
+                if(!vis[0][j] && board[0][j]=='O'){
+                    dfs(0,j,board,vis);
+                }
+        }
+        for(int j=0;j<m;j++){
+                if(!vis[n-1][j] && board[n-1][j]=='O'){
+                    dfs(n-1,j,board,vis);
+                }
+        }
+        for(int i=0;i<n;i++){
+            if(!vis[i][m-1] && board[i][m-1]=='O'){
+                    dfs(i,m-1,board,vis);
+                }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(vis[i][j]==0 ){board[i][j]='X';}
+            }
+        }
+        
     }
 };
