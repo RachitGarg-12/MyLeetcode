2f7b1dd359cc13ab1f2ad778f4b5aa402cace3b4
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-16 14:42:36 +0530

    Time: 619 ms (53.30%), Space: 165.1 MB (14.38%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
     long long maximumImportance(int n, vector<vector<int>>& roads) {
-        vector<vector<int>> adj(n);
-        vector<pair<int,int>> sz;
-        for(auto i:roads){
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
+       vector<vector<int>>adj(n);
+        for(int i=0;i<roads.size();i++)
+        {
+            int u=roads[i][0];
+            int v=roads[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
-        for(int i=0;i<n;i++){
-            sz.push_back({adj[i].size(),i});
+        vector<pair<int,int>>temp;
+        for(int i=0;i<n;i++)
+        {
+            temp.push_back({adj[i].size(),i});
         }
-        sort(sz.begin(),sz.end());
-        int val=n;
-        unordered_map<int,int>value;
-        int m=sz.size();
-        for(int i=m-1;i>=0;i--){
-            value[sz[i].second]=val;val--;
+        sort(temp.begin(),temp.end());
+        unordered_map<int,int>mp;
+        for(int i=0;i<temp.size();i++)
+        {
+            mp[temp[i].second]=i+1;
         }
-        long long ans=0;
-        for(auto i:roads){
-            ans+= value[i[0]]+value[i[1]];
+        long long int ans=0;
+        for(int i=0;i<roads.size();i++)
+        {
+            int u=roads[i][0];
+            int v=roads[i][1];
+            ans=ans+mp[u]+mp[v];
         }
         return ans;
     }
