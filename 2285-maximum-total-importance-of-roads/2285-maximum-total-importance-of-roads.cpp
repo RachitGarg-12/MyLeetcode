3018d063b8e2e1423f7b5074c6ec98dc4e8f3e3e
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-16 14:39:00 +0530

    Time: 717 ms (44.84%), Space: 180.6 MB (7.11%) - LeetHub

--- 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
+++ 2285-maximum-total-importance-of-roads/2285-maximum-total-importance-of-roads.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
-    long long ans=0;
-    vector<int> value;
-    void dfs(int i,vector<vector<int>> &adj,vector<bool> &vis){
-        vis[i]=true;
-        for(auto j:adj[i]){
-            if(!vis[j]){
-               dfs(j,adj,vis);
-            }
-            ans+=(long long)value[i]+value[j];
-        }
-    }
     long long maximumImportance(int n, vector<vector<int>>& roads) {
         vector<vector<int>> adj(n);
+        vector<pair<int,int>> vis;
         vector<pair<int,int>> sz;
         for(auto i:roads){
             adj[i[0]].push_back(i[1]);
@@ -23,17 +13,15 @@ public:
         }
         sort(sz.begin(),sz.end());
         int val=n;
-        value.resize(n);
+        vector<int> value(n);
         int m=sz.size();
         for(int i=m-1;i>=0;i--){
             value[sz[i].second]=val;val--;
         }
-        vector<bool> vis(n,false);
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,adj,vis);
-            }
+        long long ans=0;
+        for(auto i:roads){
+            ans+= value[i[0]]+value[i[1]];
         }
-       return ans/2; 
+        return ans;
     }
 };
