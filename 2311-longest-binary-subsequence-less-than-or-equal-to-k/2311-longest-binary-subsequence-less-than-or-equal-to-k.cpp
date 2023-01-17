3c01ceda2cf5db67104155bfc267626241bc9df4
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-17 12:22:26 +0530

    Time: 2 ms (80.00%), Space: 7.3 MB (12.70%) - LeetHub

--- 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2311-longest-binary-subsequence-less-than-or-equal-to-k/2311-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-    int longestSubsequence(string s, int k) {
-        int n=s.size();
-        int val=0,pow=1;
-        int cnt=0;
-        for(int i=n-1;i>=0 && val+pow<=k;i--){
-            if(s[i]=='1'){
-               val+=pow;
-                cnt++;
-            }
-            pow<<=1;
-        }
+   int solve(string &s,int index,int power,long long score,int& k,vector<int>& dp){
+        if(index<0)return 0;
+        if(dp[index]!=-1)return dp[index];
         
-        return count(s.begin(),s.end(),'0')+cnt;
+        int include=0;
+        if(s[index]=='1'){
+            if(score+pow(2,power)<=k){
+                include=1+solve(s,index-1,power+1,score+ pow(2,power),k,dp);
+            }
+        }else{
+            include=1+solve(s,index-1,power+1,score,k,dp);
+        }
+        int notinclude=solve(s,index-1,power,score,k,dp);
+        return dp[index]=max(include,notinclude);
+    }
+    int longestSubsequence(string s, int k) {
+        int n=s.length();
+        vector<int> dp(n+1,-1);
+        int power=0;
+        long long score=0;
+        return solve(s,n-1,power,score,k,dp);
     }
 };
