1c8f72f71cb177d9d167b318603ab3405e197a6d
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-20 00:46:52 +0530

    Time: 18 ms (70.15%), Space: 10.3 MB (20.31%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        int rows = matrix.size();
-        int cols = matrix.size();
-        for(int i = 1; i < rows; i++){
-            for(int j = 0; j < cols; j++){
-                int rowUp = matrix[i-1][j];
-                int leftUp = j>0?matrix[i-1][j-1]:INT_MAX;
-                int rightUp = j<cols-1?matrix[i-1][j+1]:INT_MAX;
-                matrix[i][j] += min(rowUp, min(leftUp, rightUp));
+        int n=matrix.size();int m=matrix[0].size();int ans=INT_MAX;
+        vector<vector<int>> dp(n,vector<int> (m,INT_MAX));
+        for(int i=0;i<m;i++){
+          dp[0][i]=matrix[0][i];
+          if(n==1){ans=min(ans,dp[0][i]);}
+        }
+        //dp[i][j] is the min sum reach (i,j) from any cell in the first row
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                int left=j>0?dp[i-1][j-1]:INT_MAX;
+                int right=j<m-1?dp[i-1][j+1]:INT_MAX;
+                dp[i][j]=matrix[i][j]+min(dp[i-1][j],min(left,right));
+                if(i==n-1){ans=min(ans,dp[i][j]);}
             }
         }
-        int answer = INT_MAX;
-        for(int j=0;j < cols; j++){
-            answer = min(answer, matrix[rows-1][j]);
-        }
-        return answer;       
+        
+        return ans;     
     }
 };
