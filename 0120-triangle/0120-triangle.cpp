531b587ac325461ada793a7edab45565bef77a9d
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-20 00:32:47 +0530

    Time: 9 ms (48.01%), Space: 9.1 MB (11.54%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
-    int n;
-    int minimumTotal(vector<vector<int>>& triangle) {
-        n=triangle.size();
+    int minimumTotal(vector<vector<int>>& a) {
+        int n=a.size();
         vector<vector<int>> dp(n,vector<int>(n,0));
-        dp[0][0]=triangle[0][0];
-        for(int i=1;i<n;i++){dp[i][0]=triangle[i][0]+dp[i-1][0];}
-        int ans=dp[n-1][0];
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=i;j++){
-                int left=dp[i-1][j-1];
-                int up=1e9;
-                if(j!=i){up=dp[i-1][j];}
-                dp[i][j]=triangle[i][j]+min(left,up);
-                if(i==n-1){ans=min(ans,dp[i][j]);}
+        for(int j=0;j<n;j++){
+            dp[n-1][j]=a[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                dp[i][j]=a[i][j]+min(dp[i+1][j],dp[i+1][j+1]);
             }
         }
-        
-        return ans;
+        return dp[0][0];
     }
 };
