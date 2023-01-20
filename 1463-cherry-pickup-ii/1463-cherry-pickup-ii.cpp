1c7ce88dc77aab51d947a020af83cd31d10335a8
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-20 18:08:22 +0530

    Time: 280 ms (19.85%), Space: 17.1 MB (22.76%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -2,29 +2,34 @@ class Solution {
 public:
     int n,m;
     vector<vector<vector<int>>> dp;
-    int f(int i,int j1,int j2,vector<vector<int>>& a,vector<vector<vector<int>>> &dp){
-        if(j1<0 || j1>=m || j2<0 || j2>=m){return -1e8;}
-        if(i==n-1){
-            if(j1==j2){return a[i][j1];}
-            else{return a[i][j1]+a[i][j2];}
-        }
-        if(dp[i][j1][j2]!=-1){return dp[i][j1][j2];}
-        int maxi=0;
-        for(int k=-1;k<2;k++){
-            for(int j=-1;j<2;j++){
-               if(j1==j2){
-                   maxi=max(maxi,a[i][j1]+f(i+1,j1+k,j2+j,a,dp));
-               } 
-                else{
-                   maxi=max(maxi,a[i][j1]+a[i][j2]+f(i+1,j1+k,j2+j,a,dp)); 
-                }
-            }
-        }
-        return dp[i][j1][j2]=maxi;
-    }
     int cherryPickup(vector<vector<int>>& a) {
         n=a.size();m=a[0].size();
-        dp.resize(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        return f(0,0,m-1,a,dp);
+        dp.resize(n,vector<vector<int>>(m,vector<int>(m,0)));
+        for(int i=0;i<m;i++){
+            for(int j=0;j<m;j++){
+                if(i==j){dp[n-1][i][j]=a[n-1][i];}
+                else{dp[n-1][i][j]=a[n-1][i]+a[n-1][j];}
+            }    
+        }
+        
+        for(int i=n-2;i>=0;i--){
+            for(int j1=0;j1<m;j1++){
+                for(int j2=0;j2<m;j2++){
+                    int maxi=0;
+                  for(int k=-1;k<2;k++){
+                    for(int j=-1;j<2;j++){ 
+                      if(j1+k>=0 && j1+k<m && j2+j>=0 && j2+j<m){
+                       if(j1==j2){maxi=max(maxi,a[i][j1]+dp[i+1][j1+k][j2+j]);}
+                       else{
+                        maxi=max(maxi,a[i][j1]+a[i][j2]+dp[i+1][j1+k][j2+j]);
+                           }
+                      }
+                }
+            }
+               dp[i][j1][j2]=maxi;   
+        }
+      }
+    }
+        return dp[0][0][m-1];
     }
 };
