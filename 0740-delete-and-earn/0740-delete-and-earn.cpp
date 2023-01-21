176ae8a071167f94247ce9dbf4c5eb3fa5c2f0fc
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-22 00:11:57 +0530

    Time: 16 ms (70.21%), Space: 16.4 MB (20.37%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& a) {
+        vector<int> dp(10001,0);
+        vector<int> ar(10001,0);int ans=0;
+        for(int i:a) ar[i]+=i;   //we set all element prersent by their value in arr
+        dp[0]=0;dp[1]=ar[1];
+        for(int i=2;i<10001;i++){
+            dp[i]=max(dp[i-2]+ar[i],dp[i-1]);
+        }
+        return dp[10000];        
+    }
+};
