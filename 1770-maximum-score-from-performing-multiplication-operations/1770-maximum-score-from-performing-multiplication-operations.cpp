2cfe90959e3f063f851b77c820ffdb49e34f6e97
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-22 14:46:49 +0530

    Time: 228 ms (51.36%), Space: 83.7 MB (49.10%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -1,24 +1,16 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    
-    int solve(int i, int n, int j, vector<int> &nums, vector<int> &M){
-        
-        if (j == M.size()) return 0;
+    int solve(int i, int n, int j, vector<int> &nums, vector<int> &multipliers){
+        if (j == multipliers.size()) return 0;
         if (dp[i][j] != INT_MIN) return dp[i][j];
-        
-        // Left Side
-        int left = solve(i + 1, n, j + 1, nums, M) + (nums[i] * M[j]);
-        
-        // Right Side
-        int right = solve(i, n, j + 1, nums, M) + (nums[(n - 1) - (j - i)] * M[j]);
-        
+        int left = solve(i + 1, n, j + 1, nums, multipliers) + (nums[i] * multipliers[j]);
+        int right = solve(i, n, j + 1, nums, multipliers) + (nums[(n - 1) - (j - i)] * multipliers[j]);
         return dp[i][j] = max(left, right);
     }
-    
-    int maximumScore(vector<int>& nums, vector<int>& M) {   
-        int n = nums.size(), m = M.size();
+    int maximumScore(vector<int>& nums, vector<int>& multipliers) {   
+        int n = nums.size(), m = multipliers.size();
         dp.resize(m + 1, vector<int>(m + 1, INT_MIN));
-        return solve(0, n, 0, nums, M);
+        return solve(0, n, 0, nums, multipliers);
     }
 };
