a9f2b21ff568587bc7ab1c5c374d8f553e7bce7b
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-23 09:40:59 +0530

    Time: 183 ms (84.02%), Space: 60.8 MB (86.87%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        unordered_map<int,int> m,l;
-        for(auto i:trust){
-            m[i[0]]=i[1];
-            l[i[1]]++;
+        vector<int>inDegree(n + 1, 0);
+        vector<int>outDegree(n + 1, 0);
+        for (vector<int>&currTrust : trust)
+        {
+            int trustedPerson = currTrust[1];
+            int trustingPerson = currTrust[0];
+            inDegree[trustedPerson]++;
+            outDegree[trustingPerson]++;
         }
-        for(int i=1;i<=n;i++){
-            if(m[i]==0){
-              if(l[i]!=n-1){return -1;}
-                return i;}
+        //============================================================
+        for (int person = 1; person <= n; person++)
+        {
+            if ((inDegree[person] == (n - 1)) && (outDegree[person] == 0))
+                return person;
         }
+        //===============================================================
         return -1;
     }
 };
