1a2f46752fc7ab3aa80303fdbaf7ef381c79d0f7
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-28 10:24:35 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -24,14 +24,12 @@ class Solution
         for(int i=wt[0];i<=W;i++){prev[i]=val[0];}
         
         for(int i=1;i<n;i++){
-            vector<int> cur(W+1,0);
-            for(int j=0;j<=W;j++){
+            for(int j=W;j>=0;j--){
                 int nottake= prev[j];
                 int take=INT_MIN;
                 if(wt[i]<=j){take=val[i]+prev[j-wt[i]];}
-                cur[j]=max(take,nottake);
+                prev[j]=max(take,nottake);
             }
-            prev=cur;
         }
         return prev[W];
     }
