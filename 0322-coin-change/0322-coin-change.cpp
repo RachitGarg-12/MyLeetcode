764ac783121266d1cf5437f43982fd50a8dfedb3
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-28 11:04:39 +0530

    Time: 82 ms (85.76%), Space: 10.5 MB (92.40%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n=coins.size();
-        vector<vector<int>> dp(n+1,vector<int>(amount+1,0));
+        int dp[n][amount+1];
         
         for(int i=0;i<=amount;i++){
             if((i%coins[0])==0){dp[0][i]=i/coins[0];}
