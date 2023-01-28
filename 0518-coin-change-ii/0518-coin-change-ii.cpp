c03a1b1c5e743da1f987ac5f14a684f4e493b519
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-28 12:24:32 +0530

    Time: 53 ms (34.75%), Space: 18.5 MB (48.77%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,21 +1,31 @@
 class Solution {
 public:
-    int f(int i,int amount,vector<int>& coins,vector<vector<int>> &dp){
-        if(amount==0){return 1;}
-        if(i==0){
-            if(amount%coins[0]==0){return 1;}
-            return 0;
-        }
-        if(dp[i][amount]!=-1){return dp[i][amount];}
-        int nottake=f(i-1,amount,coins,dp);
-        int take=0;
-        if(coins[i]<=amount){take=f(i,amount-coins[i],coins,dp);}
-        return dp[i][amount]=take+nottake;
-    }
-    int change(int amount, vector<int>& coins) {
+    // int f(int i,int amount,vector<int>& coins,vector<vector<int>> &dp){
+    //     if(amount==0){return 1;}
+    //     if(i==0){
+    //         if(amount%coins[0]==0){return 1;}
+    //         return 0;
+    //     }
+    //     if(dp[i][amount]!=-1){return dp[i][amount];}
+    //     int nottake=f(i-1,amount,coins,dp);
+    //     int take=0;
+    //     if(coins[i]<=amount){take=f(i,amount-coins[i],coins,dp);}
+    //     return dp[i][amount]=take+nottake;
+    // }
+    int change(int target, vector<int>& coins) {
         int n=coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        
-        return f(n-1,amount,coins,dp);
+        vector<vector<int>> dp(n,vector<int>(target+1,0));
+        for(int i=0;i<=target;i++){
+            if(i%coins[0]==0){dp[0][i]=1;}
+        }
+        for(int i=1;i<n;i++){
+            for(int amount=0;amount<=target;amount++){
+                int nottake=dp[i-1][amount];
+                int take=0;
+                if(coins[i]<=amount){take=dp[i][amount-coins[i]];}
+                dp[i][amount]=take+nottake;                
+            }
+        }
+        return dp[n-1][target];
     }
 };
