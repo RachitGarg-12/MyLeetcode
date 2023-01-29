68f7ed176a28586c1f502e09a252d7b66a8cf582
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-01-29 13:37:58 +0530

    Time: 40 ms (30.63%), Space: 18.9 MB (7.30%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,17 +1,25 @@
 class Solution {
 public:
-int n,m;string a,b;
-int dp[1001][1001];
-int rec(int i,int j){
-     if(i<0 || j<0)return 0;
-    if(dp[i][j]!=-1)return dp[i][j];
-    if(a[i]==b[j]){return 1+rec(i-1,j-1);}
-    return dp[i][j]=max(rec(i-1,j),rec(i,j-1));
-                   
-}
-    int longestCommonSubsequence(string text1, string text2) {
-        a=text1,b=text2;n=a.size(),m=b.size();
-        memset(dp,-1,sizeof(dp));
-        return rec(n-1,m-1);
+    int longestCommonSubsequence(string s1, string s2) {
+        int x=s1.size(),y=s2.size();
+        vector<vector<int>> dp(x+1,vector<int>(y+1,0));
+        bool b=false;
+        // dp[i][j]represents lcs in s1(0..i) && s2(0..j) so if a particular (0,i) || (i,0) is equal then all i after that will have ans 1 and can only have one as we have 1 element
+        for(int i=0;i<y;i++){
+            if(s1[0]==s2[i]){b=true;}
+            if(b==true){dp[0][i]=1;}
+        }
+        b=false;
+        for(int i=0;i<x;i++){
+            if(s2[0]==s1[i]){b=true;}
+            if(b){dp[i][0]=1;}
+        }
+        for(int i=1;i<x;i++){
+            for(int j=1;j<y;j++){
+               if(s1[i]==s2[j]){dp[i][j]=1+dp[i-1][j-1];}
+               else{dp[i][j]=max(dp[i-1][j],dp[i][j-1]);}                
+            }
+        }
+        return dp[x-1][y-1];
     }
 };
