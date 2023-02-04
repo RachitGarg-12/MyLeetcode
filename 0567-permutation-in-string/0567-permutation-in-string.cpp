8813f3113db1962c282c3aed9b8c65df3a199b10
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-04 14:44:47 +0530

    Time: 11 ms (58.98%), Space: 7.4 MB (45.48%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,20 +1,37 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        int k=s1.size(),n=s2.size();
-        if(k>n){return false;}
-        vector<int> m1(26),m2(26);
-        for(auto i:s1){m1[i-'a']++;}
+        int n = s1.length(), m = s2.length();
+        if(n>m) return false;
+        
+        vector<int> m1(26,0), m2(26,0);
         for(int i=0;i<n;i++){
-            if(i<k-1){m2[s2[i]-'a']++;}
-            else{
-                if(i!=k-1){m2[s2[i-k]-'a']--;}
-                m2[s2[i]-'a']++;
-                bool b=true;
-                for(int i=0;i<26;i++){if(m1[i]!=m2[i]){b=false;break;}}
-                if(b){return true;}
-            }
+            m1[s1[i]-'a']++;
+            m2[s2[i]-'a']++;
         }
-       return false; 
+        
+        int count = 0;
+        for(int i=0;i<26;i++)
+            if(m1[i]==m2[i]) count++;
+        
+        for(int i=0;i<m-n;i++){
+            if(count==26) 
+                return true;
+            
+            int l = s2[i]-'a', r = s2[n+i]-'a';
+            m2[l]--;
+            if(m2[l]==m1[l]) 
+                count++;
+            else if(m2[l]==m1[l]-1) 
+                count--;
+            
+            m2[r]++;
+            if(m2[r]==m1[r]) 
+                count++;
+            else if(m2[r]==m1[r]+1) 
+                count--;
+        }
+        
+        return count==26;
     }
 };
