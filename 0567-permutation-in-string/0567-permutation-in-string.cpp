faa595a9dfd07049a5ad81c480f9a4f3a0c29d98
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-04 12:22:15 +0530

    Time: 13 ms (52.81%), Space: 7.5 MB (28.67%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -3,16 +3,16 @@ public:
     bool checkInclusion(string s1, string s2) {
         int k=s1.size(),n=s2.size();
         if(k>n){return false;}
-        sort(s1.begin(),s1.end());
-        string cur="";
+        unordered_map<char,int> m1,m2;
+        for(auto i:s1){m1[i]++;}
         for(int i=0;i<n;i++){
-            if(i<k-1){cur+=s2[i];}
+            if(i<k-1){m2[s2[i]]++;}
             else{
-                if(i!=k-1){cur.erase(0,1);}
-                cur+=s2[i];
-                string temp=cur;
-                sort(temp.begin(),temp.end());
-                if(temp==s1){return true;}                
+                if(i!=k-1){m2[s2[i-k]]--;}
+                m2[s2[i]]++;
+                bool b=true;
+                for(auto i:s1){if(m1[i]!=m2[i]){b=false;break;}}
+                if(b){return true;}
             }
         }
        return false; 
