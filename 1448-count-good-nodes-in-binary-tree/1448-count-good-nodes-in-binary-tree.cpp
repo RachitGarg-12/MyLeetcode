8c715849c2df529946152a9bb76a8d69a907a5e6
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-06 19:02:45 +0530

    Time: 150 ms (55.28%), Space: 86.4 MB (14.31%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -11,42 +11,14 @@
  */
 class Solution {
 public:
-   map<TreeNode*,bool> m;
+    int f(TreeNode* cur,int mx){
+        int ans=0;
+        if(cur->val>=mx){ans++;mx=cur->val;}
+        if(cur->left!=NULL){ans+=f(cur->left,mx);}
+        if(cur->right!=NULL){ans+=f(cur->right,mx);}
+        return ans;
+    }
     int goodNodes(TreeNode* root) {
-        if(!root)
-            return 0;
-        
-        queue<pair<TreeNode*,int>> q;
-        q.push({root,root->val});
-        q.push({NULL,-1});
-        int res = 1;
-        
-        // BFS
-        while(!q.empty()){
-            auto curr = q.front();
-            q.pop();
-            TreeNode* node = curr.first;
-            int mx = curr.second;
-            
-            if(node){
-                if(node->left){
-                    if(mx<=node->left->val){
-                        res++;
-                    }
-                    q.push({node->left,max(mx,node->left->val)});
-                }
-                if(node->right){
-                    if(mx<=node->right->val){
-                        res++;
-                    }
-                    q.push({node->right,max(mx,node->right->val)});
-                }
-            }
-            else if(!q.empty()){
-                q.push({NULL,-1});
-            }
-        }
-        
-        return res;
+        return f(root,root->val);
     }
 };
