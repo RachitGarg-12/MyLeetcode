196fe67884fb624e11693cd0f1af7def151b9148
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-06 11:21:40 +0530

    Time: 53 ms (51.62%), Space: 30.1 MB (36.54%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
-    int f(int i,int j,string s,string t,vector<vector<int>> &dp){
+    int f(int i,int j,string &s,string &t,vector<vector<int>> &dp){
         if(j<0){return 1;}
-        if(i<j){return 0;}
+        if(i<0){return 0;}
         if(dp[i][j]!=-1){return dp[i][j];}
         int ntake=f(i-1,j,s,t,dp);
         int take=0;
@@ -11,7 +11,18 @@ public:
     }
     int numDistinct(string s, string t) {
         int n=s.size(),m=t.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return f(n-1,m-1,s,t,dp);
+        vector<vector<double>> dp(n+1,vector<double>(m+1,0));
+        for(int j=0;j<=m;j++){dp[0][j]=0;}
+        for(int i=0;i<=n;i++){dp[i][0]=1;}
+            
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+               double ntake=dp[i-1][j];
+               double take=0;
+               if(s[i-1]==t[j-1]){take=dp[i-1][j-1];}
+               dp[i][j]=take+ntake;                
+            }
+        }
+        return (int)dp[n][m];
     }
 };
