574b363ed1b471e6be470edb64991f427a4303d7
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-08 19:28:13 +0530

    Time: 10 ms (81.54%), Space: 9.1 MB (41.96%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -14,7 +14,21 @@ public:
     }
     int minDistance(string s1, string s2) {
         n=s1.size();m=s2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        return f(n-1,m-1,s1,s2,dp);
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+        for(int j=0;j<=m;j++){dp[0][j]=j;}
+        for(int i=0;i<=n;i++){dp[i][0]=i;}        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]){dp[i][j]=dp[i-1][j-1];}
+                else{
+                int ans=1e9;
+                ans=min(ans,1+dp[i][j-1]);//insert cur char of s2 to  s1
+                ans=min(ans,1+dp[i-1][j]);//delete cur char of s1
+                ans=min(ans,1+dp[i-1][j-1]);//replace cur char of s1 to match with s2
+                dp[i][j]=ans;   
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
