4e3dfb40ec8774937dcac8810f3073eeea7443b9
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-10 16:01:13 +0530

    Time: 26 ms (92.86%), Space: 11.2 MB (69.60%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,27 +1,9 @@
 class Solution {
 public:
-    bool f(int i,int j,string &s,string &p,vector<vector<int>> &dp){
-        if(i<0){
-            if(j>=0){
-                for(int k=0;k<=j;k++){
-                    if(p[k]!='*'){return false;}
-                }
-                return true;
-            }
-            else{return true;}
-        }
-       if(j<0){
-           if(i<0){return true;}
-           return false;
-       } 
-        if(dp[i][j]!=-1){return dp[i][j];}
-       if(s[i]==p[j] || p[j]=='?'){return dp[i][j]=f(i-1,j-1,s,p,dp);}
-       else if(p[j]=='*'){return  dp[i][j]=(f(i,j-1,s,p,dp) |f(i-1,j,s,p,dp));}
-       return dp[i][j]=false;
-    }
     bool isMatch(string s, string p) {
         int n=s.size(),m=p.size();
-        vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
+        int dp[n+1][m+1];
+        memset(dp,false,sizeof(dp));
         for(int i=1;i<=m;i++){
             if(p[i-1]=='*'){dp[0][i]=true;}
             else{break;}
