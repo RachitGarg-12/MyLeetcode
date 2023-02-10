4a71ef2698dd94f57c33966441cd6e39b430b8e6
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-11 01:26:58 +0530

    Time: 8 ms (44.86%), Space: 11.8 MB (36.58%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-   int dfs(int i , int turn , vector<int>& prices , vector<vector<int>>& dp){
-        if(i >= prices.size()) return 0;
-        // 0 for buy
-        // 1 for sell 
-        // 2 for last day sold
-        if(dp[i][turn] != -1){
-            return dp[i][turn];
+   // int dfs(int i , int turn , vector<int>& prices , vector<vector<int>>& dp){
+   //      if(i >= prices.size()) return 0;
+   //      // 0 for buy
+   //      // 1 for sell 
+   //      // 2 for last day sold
+   //      if(dp[i][turn] != -1){
+   //          return dp[i][turn];
+   //      }
+   //      if(turn == 0){
+   //          return dp[i][turn] = max(-prices[i] + dfs(i + 1 , 1 , prices , dp) , dfs(i + 1 , 0 , prices , dp));
+   //      }
+   //      else if(turn == 1){
+   //          return dp[i][turn] = max(prices[i] + dfs(i + 1 , 2 , prices , dp) , dfs(i + 1 , 1 , prices , dp));
+   //      }
+   //      else{
+   //          return dp[i][turn] = dfs(i + 1 , 0 , prices , dp);
+   //      }
+   //  }
+    int maxProfit(vector<int>& a) {
+        int n=a.size();
+        vector<vector<int>> dp(n+2,vector<int>(2,0));
+        dp[n][0]=dp[n][1]=0;
+        for(int i=n-1;i>=0;i--){
+            dp[i][0]=max(a[i]+dp[i+2][1],dp[i+1][0]);
+            dp[i][1]=max((-a[i])+dp[i+1][0],dp[i+1][1]);
         }
-        if(turn == 0){
-            return dp[i][turn] = max(-prices[i] + dfs(i + 1 , 1 , prices , dp) , dfs(i + 1 , 0 , prices , dp));
-        }
-        else if(turn == 1){
-            return dp[i][turn] = max(prices[i] + dfs(i + 1 , 2 , prices , dp) , dfs(i + 1 , 1 , prices , dp));
-        }
-        else{
-            return dp[i][turn] = dfs(i + 1 , 0 , prices , dp);
-        }
-    }
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>> dp(n + 1 , vector<int>(3 , -1));
-        return dfs(0 , 0 , prices , dp);
+        return dp[0][1];
     }
 };
