3e7624a0cd66692738c5f951c8a51d36355cfc6b
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-10 16:01:32 +0530

    Time: 33 ms (91.18%), Space: 11.3 MB (64.91%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -3,12 +3,12 @@ public:
     bool isMatch(string s, string p) {
         int n=s.size(),m=p.size();
         int dp[n+1][m+1];
-        memset(dp,false,sizeof(dp));
+        memset(dp,0,sizeof(dp));
         for(int i=1;i<=m;i++){
-            if(p[i-1]=='*'){dp[0][i]=true;}
+            if(p[i-1]=='*'){dp[0][i]=1;}
             else{break;}
         }
-        dp[0][0]=true;
+        dp[0][0]=1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(s[i-1]==p[j-1] || p[j-1]=='?'){dp[i][j]=dp[i-1][j-1];}
