d747766ebf12a85d4433d345ee80a3d1a55feb13
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-10 18:51:07 +0530

    Time: 161 ms (85.86%), Space: 77.9 MB (73.74%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,22 +1,35 @@
 class Solution {
 public:
-    int n;
-    int dp[100005][2][3];
-    int f(int i,int buy,vector<int>& a,int take){
-        if(i==n || take==0){return 0;}
-        if(dp[i][buy][take]!=-1){return dp[i][buy][take];}
-        int profit=0;
-        if(buy){
-            profit=max((-a[i])+f(i+1,0,a,take),f(i+1,1,a,take));
-        }
-        else{
-            profit=max(a[i]+f(i+1,1,a,take-1),f(i+1,0,a,take));
-        }
-        return dp[i][buy][take]=profit;
-    }
+    // int f(int i,int buy,vector<int>& a,int take){
+    //     if(i==n || take==0){return 0;}
+    //     if(dp[i][buy][take]!=-1){return dp[i][buy][take];}
+    //     int profit=0;
+    //     if(buy){
+    //         profit=max((-a[i])+f(i+1,0,a,take),f(i+1,1,a,take));
+    //     }
+    //     else{
+    //         profit=max(a[i]+f(i+1,1,a,take-1),f(i+1,0,a,take));
+    //     }
+    //     return dp[i][buy][take]=profit;
+    // }
     int maxProfit(vector<int>& a) {
-        n=a.size();
-        memset(dp,-1,sizeof(dp));
-        return f(0,1,a,2);
+        int n=a.size();
+        int dp[n+1][2][3];
+        memset(dp,0,sizeof(dp));
+        for(int i=n-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                for(int take=2;take>=1;take--){
+                    int profit=0;
+                    if(buy){
+                        profit=max((-a[i])+dp[i+1][0][take],dp[i+1][1][take]);
+                    }
+                    else{
+                        profit=max(a[i]+dp[i+1][1][take-1],dp[i+1][0][take]);
+                    }
+                   dp[i][buy][take]=profit;
+                }
+            }
+        }
+        return dp[0][1][2];
     }
 };
