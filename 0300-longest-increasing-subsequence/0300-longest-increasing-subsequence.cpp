7ac93b59a4da1da5a2f40dec4ae25743c5f9abc4
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-11 11:12:29 +0530

    Time: 363 ms (50.42%), Space: 37.8 MB (25.14%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int n;
-    int f(int i,vector<int>& a,int j,vector<vector<int>> &dp){
-        if(i==n || i<j){return 0;}
-        if(dp[i][j]!=-1){return dp[i][j];}
-        int ans=f(i+1,a,j,dp);
-        if(a[i]>a[j]){ans=max(ans,1+f(i+1,a,i,dp));}
-        return dp[i][j]=ans;
-    }
+    // int f(int i,vector<int>& a,int j,vector<vector<int>> &dp){
+    //     if(i==n || i<j){return 0;}
+    //     if(dp[i][j]!=-1){return dp[i][j];}
+    //     int ans=f(i+1,a,j,dp);
+    //     if(a[i]>a[j]){ans=max(ans,1+f(i+1,a,i,dp));}
+    //     return dp[i][j]=ans;
+    // }
     int lengthOfLIS(vector<int>& a) {
         n=a.size();
         int dp[n+1][n+1];
         memset(dp,0,sizeof(dp));
         for(int i=n-1;i>=0;i--){
-            for(int j=n-1;j>=0;j--){
+            for(int j=i;j>=0;j--){
                 if(i<j){continue;}
                 int ans=dp[i+1][j];
                 if(a[i]>a[j]){ans=max(ans,1+dp[i+1][i]);}
