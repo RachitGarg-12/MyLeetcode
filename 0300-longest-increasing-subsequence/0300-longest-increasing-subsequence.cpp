0c5ad7e71caa9747a720d5ee794f4c710fce5948
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-11 12:22:42 +0530

    Time: 11 ms (86.99%), Space: 10.5 MB (77.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    int n;
-    // int f(int i,vector<int>& a,int j,vector<vector<int>> &dp){
-    //     if(i==n || i<j){return 0;}
-    //     if(dp[i][j]!=-1){return dp[i][j];}
-    //     int ans=f(i+1,a,j,dp);
-    //     if(a[i]>a[j]){ans=max(ans,1+f(i+1,a,i,dp));}
-    //     return dp[i][j]=ans;
-    // }
+    //lis in O(nlogn) using binary search and overwriting new lis
     int lengthOfLIS(vector<int>& a) {
-        n=a.size();
-        vector<int> dp(n,1);
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<i;j++){
-                if(a[i]>a[j]){
-                    dp[i]=max(1+dp[j],dp[i]);
-                }
+        int n=a.size();
+        vector<int> lis;
+        lis.push_back(a[0]);
+        for(int i=1;i<n;i++){
+            if(a[i]>lis.back()){lis.push_back(a[i]);}
+            else{
+                int ind=lower_bound(lis.begin(),lis.end(),a[i])-lis.begin();
+                lis[ind]=a[i];
             }
-            ans=max(ans,dp[i]);
         }
-        return ans;
+        return lis.size();
     }
 };
