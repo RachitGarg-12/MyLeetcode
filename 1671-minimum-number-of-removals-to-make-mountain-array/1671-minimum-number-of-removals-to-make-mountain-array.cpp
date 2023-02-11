31243a1402d984eb629784dea87824d5ef660ec4
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-11 23:50:28 +0530

    Time: 444 ms (26.54%), Space: 12.1 MB (63.15%) - LeetHub

---
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumMountainRemovals(vector<int>& a) {
+        int n=a.size();
+	    vector<int> dp1(n,1),dp2(n,1);
+	    int ans=1;
+	    for(int i=1;i<n;i++){
+	        for(int j=0;j<i;j++){
+	            if(a[i]>a[j] && dp1[j]+1>dp1[i]){
+	                dp1[i]=dp1[j]+1;
+	            }
+	        }
+	    }
+	    for(int i=n-2;i>=0;i--){
+	        for(int j=n-1;j>i;j--){
+	            if(a[i]>a[j] && dp2[j]+1>dp2[i]){
+	                dp2[i]=dp2[j]+1;
+	            }
+	        }
+	    }	    
+	    for(int i=0;i<n;i++){
+	        if(dp1[i]>1 && dp2[i]>1){ans=max(ans,dp1[i]+dp2[i]-1);}
+	    }
+	    
+	    return n-ans;        
+    }
+};
