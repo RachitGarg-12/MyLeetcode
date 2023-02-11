d899825d2fb7ac6e3d12be57676840085475d295
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-11 18:43:48 +0530

    Time: 59 ms (64.08%), Space: 8.8 MB (94.53%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.cpp
@@ -3,21 +3,26 @@ public:
     vector<int> largestDivisibleSubset(vector<int>& a) {
         int n=a.size();
         sort(a.begin(),a.end());
-        vector<int> score(n,0);
-        int mx=0,mxi=0;
-        for(int i=1;i<n;i++){
+        vector<int> dp(n,1),hash(n);
+        int mx=1,lastindex=0;
+        for(int i=0;i<n;i++){
+            hash[i]=i;
             for(int j=0;j<i;j++){
-                if(a[i]%a[j]==0 && score[j]+1>score[i]){score[i]=score[j]+1;}
+                if(a[i]%a[j]==0 && dp[j]+1>dp[i]){
+                    dp[i]=1+dp[j];
+                    hash[i]=j;
+                }
             }
-            if(score[i]>mx){mx=score[i];mxi=i;}
+            if(dp[i]>mx){
+                mx=dp[i];
+                lastindex=i;
+            }
         }
         vector<int> ans;
-        ans.push_back(a[mxi]);mx--;
-        int last=a[mxi];
-        for(int i=mxi-1;i>=0;i--){
-            if(last%a[i]==0 && score[i]==mx){
-                ans.push_back(a[i]);mx--;last=a[i];
-            }
+        ans.push_back(a[lastindex]);
+        while(hash[lastindex]!=lastindex){
+            lastindex=hash[lastindex];
+            ans.push_back(a[lastindex]);
         }
         if(ans.size()==0){ans.push_back(a[0]);}
         return ans;
