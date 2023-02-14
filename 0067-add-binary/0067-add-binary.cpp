0f27fbd87618e577b4a696277784b633b31eb984
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-14 12:27:38 +0530

    Time: 2 ms (73.96%), Space: 6.2 MB (96.98%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int n=a.size(),m=b.size();
-        int i=n-1,j=m-1;
-        int carry=0;
-        string ans="";
-        while(i>=0 && j>=0){
-            if((a[i]-'0')+(b[j]-'0')+carry==3){ans+='1';carry=1;}
-            else if((a[i]-'0')+(b[j]-'0')+carry==2){ans+='0';carry=1;}
-            else if((a[i]-'0')+(b[j]-'0')+carry==1){ans+='1';carry=0;}
-            else if((a[i]-'0')+(b[j]-'0')+carry==0){ans+='0';carry=0;}
-            i--;j--;
+        // Initialize two pointers to traverse the binary strings from right to left
+        int i = a.length()-1;
+        int j = b.length()-1;
+        string ans;
+        int carry = 0;
+        
+        // Loop until both pointers have reached the beginning of their respective strings and there is no carry-over value left
+        while(i >= 0 || j >= 0 || carry) {
+            // Add the current binary digit in string a, if the pointer is still within bounds
+            if(i >= 0) {
+                carry += a[i] - '0';
+                i--;
+            }
+            
+            // Add the current binary digit in string b, if the pointer is still within bounds
+            if(j >= 0) {
+                carry += b[j] - '0';
+                j--;
+            }
+            
+            // Calculate the next binary digit in the result by taking the remainder of the sum divided by 2
+            ans += (carry % 2 + '0');
+            
+            // Calculate the next carry-over value by dividing the sum by 2
+            carry = carry / 2;
         }
-        while(i>=0){
-            if(a[i]-'0'+carry==1){ans+='1';carry=0;}
-            else if(a[i]-'0'+carry==2){ans+='0';carry=1;}
-            else{ans+='0';carry=0;}
-            i--;
-        }
-        while(j>=0){
-            if(b[j]-'0'+carry==1){ans+='1';carry=0;}
-            else if(b[j]-'0'+carry==2){ans+='0';carry=1;}
-            else{ans+='0';carry=0;}
-            j--;
-        }
-        if(carry==1){ans+='1';}
-        reverse(ans.begin(),ans.end());
-        if(ans.size()==0){return "0";}
+        
+        // Reverse the result and return it as a string
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
