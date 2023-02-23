7faf54fc00eafd02d14bc9c8334ee89ded659ef8
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-23 12:18:16 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -6,27 +6,43 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in a directed graph.
-    bool dfs(int i,vector<int> adj[],vector<int> &vis,vector<int> &pathvis){
-        vis[i]=1;
-        pathvis[i]=1;
-        for(auto j:adj[i]){
-            if(!vis[j]){
-                if(dfs(j,adj,vis,pathvis))return true;
-            }
-            else if(pathvis[j]){return true;}
-        }
-        pathvis[i]=0;
-        return false;
-    }
-    bool isCyclic(int n, vector<int> adj[]) {
-        vector<int> vis(n,0);
-        vector<int> pathvis(n,0);
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                if(dfs(i,adj,vis,pathvis))return true;
-            }
-        }
-        return false;
+    // bool dfs(int i,vector<int> adj[],vector<int> &vis,vector<int> &pathvis){
+    //     vis[i]=1;
+    //     pathvis[i]=1;
+    //     for(auto j:adj[i]){
+    //         if(!vis[j]){
+    //             if(dfs(j,adj,vis,pathvis))return true;
+    //         }
+    //         else if(pathvis[j]){return true;}
+    //     }
+    //     pathvis[i]=0;
+    //     return false;
+    // }
+    bool isCyclic(int V, vector<int> adj[]) {
+        vector<int> indeg(V,0);
+	    queue<int> q;
+	    for(int i=0;i<V;i++){
+	        for(auto j:adj[i]){
+	            indeg[j]++;
+	        }
+	    }
+	    for(int i=0;i<V;i++){
+	        if(indeg[i]==0){q.push(i);}
+	    }
+	    vector<int> ans;
+	    while(!q.empty()){
+	        int node=q.front();
+	        ans.push_back(node);
+	        q.pop();
+	        for(auto i:adj[node]){
+	            if(indeg[i]>0){
+	                indeg[i]--;
+	                if(indeg[i]==0){q.push(i);}
+	            }
+	        }
+	    }
+	    if(ans.size()==V){return false;}
+	    return true;
     }
 };
 
