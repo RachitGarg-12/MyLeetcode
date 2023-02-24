d3e5d656890cad6873a2d00255b2255931b430e6
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-24 11:51:35 +0530

    Time: 226 ms (32.60%), Space: 61.7 MB (17.28%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -1,37 +1,30 @@
 class Solution {
 public:
-    bool dfs(int i,vector<vector<int>>& adj,vector<int> &vis,vector<int> &pathvis,vector<int> &check){
-        vis[i]=1;
-        pathvis[i]=1;
-        check[i]=0;
-        for(auto j:adj[i]){
-            if(!vis[j]){
-                if(dfs(j,adj,vis,pathvis,check)){
-                    check[i]=0;return true;
-                }
-            }
-            else if(pathvis[j]){
-                check[i]=0;return true;
-            }
-        }
-        check[i]=1;
-        pathvis[i]=0;
-        return false;
-    }
+    //using bfs toposort
     vector<int> eventualSafeNodes(vector<vector<int>>& adj) {
         int n=adj.size();
-        vector<int> vis(n,0);
-        vector<int> pathvis(n,0);
-        vector<int> check(n,0);
-        vector<int> ans;
+        vector<vector<int>> adjrev(n);
+        vector<int> indeg(n,0);
         for(int i=0;i<n;i++){
-            if(!vis[i]){
-                dfs(i,adj,vis,pathvis,check);
+            for(auto it:adj[i]){
+                adjrev[it].push_back(i);indeg[i]++;
             }
         }
+        queue<int> q;
+        vector<int> safe;
         for(int i=0;i<n;i++){
-            if(check[i]){ans.push_back(i);}
+            if(indeg[i]==0){q.push(i);}
         }
-        return ans;        
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            safe.push_back(node);
+            for(auto i:adjrev[node]){
+                indeg[i]--;
+                if(indeg[i]==0){q.push(i);}
+            }
+        }
+        sort(safe.begin(),safe.end());
+        return safe;
     }
 };
