2b861c2e98f58b79731ffbe8ee57e2cb97a0dc23
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-25 11:31:12 +0530

    Time: 1318 ms (18.83%), Space: 281.3 MB (5.06%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,38 +1,11 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        int n=beginWord.size(),l=wordList.size();
-        bool b=false;
-        unordered_map<string,vector<string>> m;
-        for(int j=0;j<l;j++){
-            string cur=wordList[j];
-            if(cur==endWord){b=true;}
-            int req=0;
-            for(int i=0;i<n;i++){
-                if(beginWord[i]!=cur[i]){req++;}
-            }
-            if(req==1){
-                m[beginWord].push_back(cur);
-                m[cur].push_back(beginWord);
-            }
+        unordered_map<string,int> pre;
+        for(auto i:wordList){
+            pre[i]=1;
         }
-        if(b==false){return 0;}
-        for(int i=0;i<l-1;i++){
-            string cur=wordList[i];
-            if(cur==beginWord){continue;}
-            for(int j=i+1;j<l;j++){
-                string sec=wordList[j];
-                if(sec==beginWord){continue;}
-                int req=0;
-                for(int k=0;k<n;k++){
-                    if(cur[k]!=sec[k]){req++;}
-                }
-                if(req==1){
-                    m[cur].push_back(sec);
-                    m[sec].push_back(cur);
-                }
-            }
-        }
+        int n=beginWord.size();
         queue<string> q;
         unordered_map<string,int> vis;
         q.push(beginWord);
@@ -41,13 +14,17 @@ public:
         while(!q.empty()){
             int sz=q.size();
             while(sz--){
-                string node=q.front();q.pop();
-                for(auto i:m[node]){
-                    if(!vis[i]){
-                        if(i==endWord){return level;}
-                        if(!vis[i]){vis[i]=1;q.push(i);}
-                    }
-                }
+            string cur=q.front();q.pop();
+            for(int i=0;i<n;i++){
+               for(char c='a';c<='z';c++){
+                   string t=cur;
+                   t[i]=c;
+                   if(!vis[t] && pre[t]){
+                       if(t==endWord){return level;}
+                       q.push(t);vis[t]=1;
+                   }
+               }
+            }
             }
             level++;
         }
