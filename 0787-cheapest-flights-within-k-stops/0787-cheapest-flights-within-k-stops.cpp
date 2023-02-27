618f062e4f9c20ea971affc353eb56920116fc31
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-02-27 16:07:12 +0530

    Time: 16 ms (98.38%), Space: 13.6 MB (57.03%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<vector<pair<int, int>>> adj(n);
-        for(auto flight : flights){
-            // flight[0] represent node i, flight[1] represent neighbor node of node i, flight[2] represent cost between node i to neighbor node
-            adj[flight[0]].push_back({flight[1], flight[2]});
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto i:flights){
+            adj[i[0]].push_back({i[1],i[2]});
         }
-        //it will store [node, cost]
-        queue<pair<int, int>> q;
-        q.push({src, 0});
-        //it will store minimum cost to reach each node
-        vector<int> minCost(n, INT_MAX);
-        int stops = 0;
-        while(!q.empty() && stops <= k){
-            int size = q.size();
-            while (size--) {
-                auto [currNode, cost] = q.front();
-                q.pop();
-                for (auto& [neighbour, price] : adj[currNode]) {
-                    if (price + cost < minCost[neighbour]){
-                        minCost[neighbour] = price + cost;
-                        q.push({neighbour, minCost[neighbour]});
-                    }
+        queue<pair<int,pair<int,int>>> pq;
+        pq.push({0,{src,0}});
+        vector<int> minCost(n,1e9);
+        minCost[src]=0;
+        while(!pq.empty()){
+            int c=pq.front().first;
+            int node=pq.front().second.first;
+            int stops=pq.front().second.second;
+            pq.pop();
+            if(stops==k+1){continue;}
+            for(auto i:adj[node]){
+                if(c+i.second<minCost[i.first]){
+                    minCost[i.first]=c+i.second;
+                    pq.push({minCost[i.first],{i.first,stops+1}});
                 }
             }
-            stops++;
         }
-        if(minCost[dst] == INT_MAX)
-            return -1;
+        if(minCost[dst]==1e9){return -1;}
         return minCost[dst];
     }
 };
