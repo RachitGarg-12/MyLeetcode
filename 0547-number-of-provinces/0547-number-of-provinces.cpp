ca726182c50764f4885c6c71665b39f2be9f37c1
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-03-06 16:54:16 +0530

    Time: 42 ms (14.63%), Space: 19.6 MB (5.01%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,24 +1,71 @@
-class Solution {
-public:
-    int n;
-    void dfs(int i,vector<int>&vis,vector<vector<int>>& isConnected){
-        vis[i]=1;
-        for(int j=0;j<n;j++){
-            if(i==j){continue;}
-            if(isConnected[i][j]==1 && !vis[j]){
-                dfs(j,vis,isConnected);
-            }
+class DisjointSet{
+    vector<int> rank,parent,size;
+    public:
+    DisjointSet(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1,0);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+            size[i]=1;
         }
     }
+
+    int findUpar(int node){
+        if(node==parent[node]){return node;}
+        return parent[node]=findUpar(parent[node]);
+    }
+
+    void unionByRank(int u,int v){
+        int upu = findUpar(u);
+        int upv = findUpar(v);
+        if(upu==upv){return;}
+        if(rank[upu]<rank[upv]){
+            parent[upu]=upv;
+        }
+        else if(rank[upu]>rank[upv]){
+           parent[upv]=upu;
+        }
+        else{
+           parent[upv]=upu;
+           rank[upu]++; 
+        }
+    } 
+    void unionBySize(int u,int v){
+        int upu = findUpar(u);
+        int upv = findUpar(v);
+        if(upu==upv){return;}
+        if(size[upu]<size[upv]){
+            parent[upu]=upv;
+            size[upv]+=size[upu];
+        }
+        else{
+            parent[upv]=upu;
+            size[upu]+=size[upv];
+        }
+    }
+};
+
+class Solution {
+public:
+    
     int findCircleNum(vector<vector<int>>& isConnected) {
-          n=isConnected.size();
-          vector<int> vis(n,0);
-         int provinces=0;
-          for(int i=0;i<n;i++){
-              if(!vis[i]){
-                  provinces++;dfs(i,vis,isConnected);
-              }
-          }
-        return provinces;
+        int n=isConnected.size();
+        vector<vector<int>> edges;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i!=j && isConnected[i][j]==1){edges.push_back({i+1,j+1});}
+            }
+        }
+        DisjointSet ds(n);
+        unordered_set<int> par;
+        for(auto i:edges){
+            ds.unionBySize(i[0],i[1]);
+        }
+        int ans=0;
+        for(int i=1;i<=n;i++){
+            if(ds.findUpar(i)==i){ans++;}
+        }
+        return ans;
     }
 };
