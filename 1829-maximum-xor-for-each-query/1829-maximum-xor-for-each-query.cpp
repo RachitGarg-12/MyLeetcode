4049b6b6d16a118349b75fb9f95954df257e10ff
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-03-12 16:26:01 +0530

    Time: 159 ms (83.88%), Space: 97.2 MB (25.61%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.cpp
@@ -2,21 +2,17 @@ class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& a, int maximumBit) {
         int n=a.size();
-        vector<int> pre(n);
-        pre[0]=a[0];
-        for(int i=1;i<n;i++){
-            pre[i]=pre[i-1]^a[i];
+        int prexor=0;
+        for(int i=0;i<n;i++){
+            prexor^=a[i];
         }
         vector<int> ans;
-        while(n-1>=0){
-            int cur=pre[n-1];
-            int i=0,k=0;
-            while(i<maximumBit){
-                if((cur&(1<<i))==0){k+=(1<<i);}
-                i++;
-            }
-            ans.push_back(k);
-            n--;
+        int mx= (1<<maximumBit)-1;
+        // for a1^a2^a3...^k=max , we have k= a1^a2^a3..^max
+        // max is (2^maximumbit)-1
+        for(int i=n-1;i>=0;i--){
+            ans.push_back(prexor^mx);
+            prexor^=a[i];
         }
         return ans;
     }
