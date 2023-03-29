5080f24417827c988a08adf055ee3cd55c61928d
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-03-29 09:24:16 +0530

    Time: 36 ms (50.54%), Space: 20.8 MB (54.63%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     int n;
-    int f(int i,int ele,vector<int>& satisfaction, vector<vector<int>> &dp){
+    int f(int i,int ele,vector<int>& a, vector<vector<int>> &dp){
         if(i>=n){return 0;}
         if(dp[i][ele]!=-1){return dp[i][ele];}
         int ans=0;
-        if(satisfaction[i]<=0){
-            ans=f(i+1,ele,satisfaction,dp);
+        if(a[i]<=0){
+            ans=f(i+1,ele,a,dp);
         }
-        ans=max(ans,satisfaction[i]*(ele+1)+f(i+1,ele+1,satisfaction,dp));
+        ans=max(ans,a[i]*(ele+1)+f(i+1,ele+1,a,dp));
         return dp[i][ele]=ans;
     }
-    int maxSatisfaction(vector<int>& satisfaction) {
-        n=satisfaction.size();
-        sort(satisfaction.begin(),satisfaction.end());
+    int maxSatisfaction(vector<int>& a) {
+        n=a.size();
+        sort(a.begin(),a.end());
         vector<vector<int>> dp(n,vector<int>(n,-1));
-        return f(0,0,satisfaction,dp);
+        return f(0,0,a,dp);
     }
 };
