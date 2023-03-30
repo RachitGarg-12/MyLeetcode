c7382dac70ec3f680c9a08233e4a1f341ba7923e
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-03-30 12:06:39 +0530

    Time: 7 ms (68.57%), Space: 8.9 MB (59.64%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -4,12 +4,9 @@ public:
         unordered_map<int,int> m;
         stack<int> s;
         for(int i=nums2.size()-1;i>=0;i--){
+            while(!s.empty() && s.top()<nums2[i]){s.pop();}
             if(s.empty()){m[nums2[i]]=-1;}
-            else{
-                while(!s.empty() && s.top()<nums2[i]){s.pop();}
-                if(s.empty()){m[nums2[i]]=-1;}
-                else{m[nums2[i]]=s.top();}
-            }
+            else{m[nums2[i]]=s.top();}
             s.push(nums2[i]);
         }
         vector<int> ans;
