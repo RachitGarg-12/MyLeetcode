9aab1cb8a5c766d3c0825fffb14a51a9a3fbfa67
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-03-31 12:04:33 +0530

    Time: 8 ms (54.78%), Space: 13.3 MB (30.51%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,44 +1,39 @@
 class Solution {
 public:
-     int bfs(vector<vector<int>>& grid,queue<pair<pair<int,int>,int>>& q,vector<vector<int>> &vis,int n,int m){
-        int ans=0;
-        
-        while(!q.empty()){
-            pair<pair<int,int>,int> p=q.front();q.pop();
-            int r=p.first.first,c=p.first.second,t=p.second;
-            ans=max(ans,t);
-            if(r-1>=0 && grid[r-1][c]==1 && vis[r-1][c]==0){
-                    vis[r-1][c]=1;q.push({{r-1,c},t+1});grid[r-1][c]=2;
-            }
-            if(r<n-1 && grid[r+1][c]==1 && vis[r+1][c]==0){                                                   vis[r+1][c]=1;q.push({{r+1,c},t+1});grid[r+1][c]=2;
-            }
-            if(c<m-1 && grid[r][c+1]==1 && vis[r][c+1]==0){
-                    vis[r][c+1]=1;q.push({{r,c+1},t+1});grid[r][c+1]=2;
-            }
-            if(c-1>=0 && grid[r][c-1]==1 && vis[r][c-1]==0){
-                vis[r][c-1]=1;q.push({{r,c-1},t+1});grid[r][c-1]=2;
-            }
-            
-        }
-         return ans;
-    }
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();int m=grid[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        queue<pair<pair<int,int>,int>> q;bool done=false;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2 && vis[i][j]==0){
-                    done=true;q.push({{i,j},0});
-                }
-            }
-        }
-        int ans=bfs(grid,q,vis,n,m);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1) return -1;
-            }}
-      
-        return done?ans:0;
-    }
-};
+		vector<pair<int,int>> dir={{-1,0},{1,0},{0,1},{0,-1}};
+		int orangesRotting(vector<vector<int>>& grid) {
+			int n=grid.size();
+			int m=grid[0].size();
+			vector<vector<int>> vis(n,vector<int>(m,0));
+			queue<pair<pair<int,int>,int>>q;
+			for(int i=0;i<n;i++){
+				for(int j=0;j<m;j++){
+					if(grid[i][j]==2){
+						q.push({{i,j},0});
+						vis[i][j]=2;
+					}
+				}
+			}
+			int time=0;
+			while(!q.empty()){
+				auto node=q.front();
+				q.pop();
+				int t=node.second;
+				time=max(time,t);
+				for(auto d:dir){
+					int new_r=node.first.first+d.first;
+					int new_c=node.first.second+d.second;
+					if(new_r<n && new_r>=0 && new_c<m && new_c>=0 && vis[new_r][new_c]!=2 && grid[new_r][new_c]==1){
+						q.push({{new_r,new_c},t+1});
+						vis[new_r][new_c]=2;
+					}    
+				}
+			}
+			for(int i=0;i<n;i++){
+				for(int j=0;j<m;j++){
+					if(grid[i][j]==1 && vis[i][j]!=2) return -1;
+				}
+			}
+			return time;
+		}
+	};
