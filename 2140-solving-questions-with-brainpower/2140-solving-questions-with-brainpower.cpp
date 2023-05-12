9a71e19cbe2b2a71f8bcbccd3139ead5dbfc84bc
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-05-12 07:29:46 +0530

    Time: 411 ms (14.83%), Space: 121.4 MB (53.71%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -3,10 +3,7 @@ public:
     int n;
     long long f(vector<vector<int>>& q,int i,vector<long long> &dp){
         if(i>=n){return 0;}
-        if(i==n-1){return q[i][0];}
-        
         if(dp[i]!=-1)return dp[i];
-        
         long long ans=f(q,i+q[i][1]+1,dp)+q[i][0];
         ans=max(ans,f(q,i+1,dp));
         return dp[i]=ans;
