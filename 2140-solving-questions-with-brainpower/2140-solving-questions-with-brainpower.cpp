aa5db101cbafca55e4a2658848dca0442e7c48e5
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-05-12 07:29:01 +0530

    Time: 417 ms (12.13%), Space: 121.6 MB (50.34%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int n;
+    long long f(vector<vector<int>>& q,int i,vector<long long> &dp){
+        if(i>=n){return 0;}
+        if(i==n-1){return q[i][0];}
+        
+        if(dp[i]!=-1)return dp[i];
+        
+        long long ans=f(q,i+q[i][1]+1,dp)+q[i][0];
+        ans=max(ans,f(q,i+1,dp));
+        return dp[i]=ans;
+    }
+    long long mostPoints(vector<vector<int>>& q) {
+        n=q.size();
+        vector<long long> dp(n,-1);
+        return f(q,0,dp);
+    }
+};
