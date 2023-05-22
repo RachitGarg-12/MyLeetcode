199ecbac16521c55639e60af4d29a41a916ac297
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-05-22 12:43:12 +0530

    Time: 43 ms (30.41%), Space: 32.6 MB (97.78%) - LeetHub

--- 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
+++ 0450-delete-node-in-a-bst/0450-delete-node-in-a-bst.cpp
@@ -12,19 +12,37 @@
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root) 
-            if(key < root->val) root->left = deleteNode(root->left, key);     //We frecursively call the function until we find the target node
-            else if(key > root->val) root->right = deleteNode(root->right, key);       
-            else{
-                if(!root->left && !root->right) return NULL;          //No child condition
-                if (!root->left || !root->right)
-                    return root->left ? root->left : root->right;    //One child contion -> replace the node with it's child
-					                                                //Two child condition   
-                TreeNode* temp = root->left;                        //(or) TreeNode *temp = root->right;
-                while(temp->right != NULL) temp = temp->right;     //      while(temp->left != NULL) temp = temp->left;
-                root->val = temp->val;                            //       root->val = temp->val;
-                root->left = deleteNode(root->left, temp->val);  //        root->right = deleteNode(root->right, temp);		
+        if(root==NULL){return root;}
+        if(root->val==key){
+            return helper(root);
+        }
+        TreeNode *cur=root;
+        while(root){
+            if(root->val>key){
+                if(root->left && root->left->val==key){
+                    root->left=helper(root->left);
+                    break;
+                }
+                else{root=root->left;}
             }
-        return root;        
+            else{
+                if(root->right && root->right->val==key){
+                    root->right=helper(root->right);
+                    break;
+                }
+                else{root=root->right;}
+            }
+        }
+        return cur;
+    }
+    
+    TreeNode* helper(TreeNode *root){
+        if(root->right==NULL){return root->left;}
+        else if(root->left==NULL){return root->right;}
+        TreeNode *largest=root->left;
+        while(largest->right){largest=largest->right;}
+        largest->right=root->right;
+        
+        return root->left;
     }
 };
