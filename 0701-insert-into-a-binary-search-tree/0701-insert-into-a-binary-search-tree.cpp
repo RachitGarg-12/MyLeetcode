948d99cd85394c09421555078c2861c00ae16fdf
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-05-22 10:59:34 +0530

    Time: 111 ms (19.30%), Space: 56.9 MB (41.98%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -13,16 +13,11 @@ class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
         if(root==NULL){return new TreeNode(val);}
-        TreeNode* cur=root;
-        while(true){
-            if(cur->val>val){
-                if(cur->left){cur=cur->left;}
-                else{cur->left=new TreeNode(val);break;}
-            }
-            else{
-                if(cur->right){cur=cur->right;}
-                else{cur->right=new TreeNode(val);break;}                
-            }
+        if(root->val>val){
+            root->left=insertIntoBST(root->left,val);
+        }
+        else{
+            root->right=insertIntoBST(root->right,val);
         }
         return root;
     }
