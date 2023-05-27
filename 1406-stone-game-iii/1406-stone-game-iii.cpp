e48b5e85122abbe72f5c16fa0fa2a0721c222906
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-05-27 09:49:34 +0530

    Time: 263 ms (63.03%), Space: 132.2 MB (72.27%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-    int n;
-    int alice(int i,int turn,vector<int>& stoneValue,vector<vector<int>> &dp){
-        if(i>=n){return 0;}
-        if(dp[i][turn]!=-1){return dp[i][turn];}
-        int ans=(turn==1)?-1e9:1e9;
-        int score=0;
-        for(int it=i;it<i+3 && it<n ;it++){
-            score+=stoneValue[it];
-            if(turn){ans=max(ans,score+alice(it+1,0,stoneValue,dp));}
-            else{ans=min(ans,alice(it+1,1,stoneValue,dp));}
+    string stoneGameIII(vector<int>& a) {
+        int n=a.size();
+        vector<int> dp(n+1,-1e9);
+        dp[n]=0;
+        for(int i=n-1;i>=0;i--){
+            int one=a[i]-dp[i+1];
+            int two=-1e9;
+            if(i+2<=n){two=a[i]+a[i+1]-dp[i+2];}
+            int three=-1e9;
+            if(i+3<=n){three=a[i]+a[i+1]+a[i+2]-dp[i+3];}
+            dp[i]=max({one,two,three});
         }
-        return dp[i][turn]=ans;
-    }
-    string stoneGameIII(vector<int>& stoneValue) {
-        n=stoneValue.size();
-        int tot=0;
-        for(auto i:stoneValue){tot+=i;}
-        vector<vector<int>> dp(n+1,vector<int>(2,-1));
-        int a=alice(0,1,stoneValue,dp);
-        int b=tot-a;
-        if(a>b){return "Alice";}
-        else if(a<b){return "Bob";}
+        if(dp[0]>0){return "Alice";}
+        else if(dp[0]<0){return "Bob";}
         return "Tie";
     }
 };
