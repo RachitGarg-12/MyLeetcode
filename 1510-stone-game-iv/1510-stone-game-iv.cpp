5a7b6c903004b6778738874722ddd082afb3c90e
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-05-27 12:04:30 +0530

    Time: 60 ms (64.17%), Space: 6.3 MB (89.72%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
-//     int f(int n,int turn){
-        
-//     }
     bool winnerSquareGame(int n) {
         vector<bool>a(n+1,true);
-        a[0]=false;a[1]=true;
+        // a[n] represents whether the person having the current chance with n number left can win or            not
+        a[0]=false;
         int mx=1;
-        for(int i=2;i<=n;i++){
+        for(int i=1;i<=n;i++){
             bool ans=false;
             for(int j=1;j*j<=i;j++){
                 if(a[i-(j*j)]==false){ans=true;break;}
