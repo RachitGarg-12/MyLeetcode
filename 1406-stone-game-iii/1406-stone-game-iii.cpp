6faa34a82df6a87b84a903fad6f2bf99ed8f8190
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-05-27 09:53:10 +0530

    Time: 261 ms (64.69%), Space: 126.3 MB (97.87%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     string stoneGameIII(vector<int>& a) {
         int n=a.size();
-        vector<int> dp(n+1,-1e9);
-        dp[n]=0;
+        vector<int> dp(3,0);
         for(int i=n-1;i>=0;i--){
-            int one=a[i]-dp[i+1];
+            int one=a[i]-dp[(i+1)%3];
             int two=-1e9;
-            if(i+2<=n){two=a[i]+a[i+1]-dp[i+2];}
+            if(i+1<n){two=a[i]+a[i+1]-dp[(i+2)%3];}
             int three=-1e9;
-            if(i+3<=n){three=a[i]+a[i+1]+a[i+2]-dp[i+3];}
-            dp[i]=max({one,two,three});
+            if(i+2<n){three=a[i]+a[i+1]+a[i+2]-dp[(i+3)%3];}
+            dp[i%3]=max({one,two,three});
         }
         if(dp[0]>0){return "Alice";}
         else if(dp[0]<0){return "Bob";}
