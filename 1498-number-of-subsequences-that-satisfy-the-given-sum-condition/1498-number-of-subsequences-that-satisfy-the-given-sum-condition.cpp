9c29de962d349987b7e2c875890cf8249a57f48d
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-05-29 13:09:24 +0530

    Time: 182 ms (15.81%), Space: 47.6 MB (98.70%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -1,17 +1,18 @@
+#define ll long long
 class Solution {
 public:
-    long long expo(long long a, long long b, long long mod) {long long res = 1; while (b > 0) {if (b & 1)res = ((res%mod) * (a%mod)) % mod; a = ((a%mod) * (a%mod)) % mod ; b = b >> 1;} return res%mod;}
+   ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a)     %mod; b = b >> 1;} return res;}
     int mod=1e9+7;
     int numSubseq(vector<int>& a, int tar) {
         int n=a.size();
         sort(a.begin(),a.end());
         int i=0,j=n-1;
-        long long ans=0;
+        ll ans=0;
         while(i<=j){
             if(a[i]+a[j]<=tar){
                 if(j==i){ans++;break;}
                 if(j-i-1>=0){
-                    long long way=expo(2,j-i,mod);
+                    ll way=expo(2,j-i,mod);
                     ans=(ans%mod+way%mod)%mod;
                 }
                 i++;
