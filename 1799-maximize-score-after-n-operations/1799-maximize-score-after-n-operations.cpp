a6b1fb25a86debd077aff370a31736c59a4a5353
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-05-30 12:36:04 +0530

    Time: 772 ms (14.63%), Space: 17.5 MB (26.37%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
@@ -1,36 +1,41 @@
 class Solution {
 public:
-int solve(vector<int>&nums,  unordered_map<vector<bool>, int>&mp, vector<bool>&visited, int operation)
-    {
+    bool isBitSet(int& num, int bitIdx){
+        //(1 << bitIdx) => only the 'bitIdx'th bit set, rest all 0'S
+        if ((num & (1 << bitIdx)) != 0) 
+            return true;
+        return false;
+    }
+    void setBit(int& num, int bitIdx){
+        num = num | (1 << bitIdx);
+    }
+    int solve(vector<int>&nums,  map<int, int>&mp, int visited, int operation){
         if (mp.count(visited)) return mp[visited]; //use stored result
-            
         int maxScore = 0;
-        for (int i = 0; i < nums.size() - 1; i++)
-        {
-            if (visited[i]) continue;
-            for (int j = i + 1; j < nums.size(); j++)
-            {
-                if (visited[j]) continue;
-                visited[i] = true;
-                visited[j] = true;
+        for (int i = 0; i < nums.size() - 1; i++){
+            if (isBitSet(visited, i)){continue;}
+            for (int j = i + 1; j < nums.size(); j++){
+                if (isBitSet(visited, j)){continue;}
+                
+                int newVisited = visited;
+                setBit(newVisited, i); //mark i as visited
+                setBit(newVisited, j); //mark j as visited
                 //=====================================================================
                 int currScore = operation * __gcd(nums[i], nums[j]);
-                int nextMaxScore = solve(nums, mp, visited, operation + 1);
+                int nextMaxScore = solve(nums, mp, newVisited, operation + 1);
                 int totalScore = currScore + nextMaxScore;
                 maxScore = max(maxScore, totalScore);
-                //======================================================================
-                visited[i] = false;
-                visited[j] = false;
+                //=====================================================================
+                //rather than unSetting we will use the same old mask in next Iteration
             }
         }
         return mp[visited] = maxScore; //store the result
     }
-    int maxScore(vector<int>& nums) 
-    {
+    int maxScore(vector<int>& nums) {
         int n = nums.size();
-        vector<bool>visited(n, false);
-        unordered_map<vector<bool>, int>mp;
-        int ans = solve(nums, mp, visited, 1);
+        int visited = 0;
+        map<int,int>mp;
+        int ans = solve(nums,mp,visited,1);
         return ans;
         
     }
