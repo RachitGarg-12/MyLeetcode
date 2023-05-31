2dac19494837a9cde134dc03e1594f8c141045d7
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-05-31 11:14:19 +0530

    Time: 710 ms (13.08%), Space: 35.8 MB (60.90%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     int l;
     int mxz,mxo;
-    int f(int i,vector<string>& strs,map<string,pair<int,int>> &freq,pair<int,int> cur,vector<map<pair<int,int>,int>>&dp){
+    int dp[601][101][101];
+    int f(int i,vector<string>& strs,map<string,pair<int,int>> &freq,int curz,int curo){
         if(i==l){return 0;}
-        if(dp[i].find(cur)!=dp[i].end()){return dp[i][cur];}
-        int ans=f(i+1,strs,freq,cur,dp);
+        if(dp[i][curz][curo]!=-1){return dp[i][curz][curo];}
+        int ans=f(i+1,strs,freq,curz,curo);
         int zer=freq[strs[i]].first,one=freq[strs[i]].second;
-        int curz=cur.first,curo=cur.second;
         if(curz+zer<=mxz && curo+one<=mxo){
-            ans=max(ans,1+f(i+1,strs,freq,{curz+zer,curo+one},dp));
+            ans=max(ans,1+f(i+1,strs,freq,curz+zer,curo+one));
         }
-        return dp[i][cur]=ans;
+        return dp[i][curz][curo]=ans;
     }
     int findMaxForm(vector<string>& strs, int m, int n) {
         mxz=m;mxo=n;
@@ -25,7 +25,7 @@ public:
             }
             freq[i]={z,o};
         }
-        vector<map<pair<int,int>,int>> dp(l+1);
-        return f(0,strs,freq,{0,0},dp);
+        memset(dp,-1,sizeof(dp));
+        return f(0,strs,freq,0,0);
     }
 };
