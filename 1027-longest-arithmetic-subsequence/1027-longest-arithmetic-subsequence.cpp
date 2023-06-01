baf43a8df2a52da6ce29e6a3fe7c47639ec1bba0
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-06-01 17:15:22 +0530

    Time: 335 ms (79.38%), Space: 141.7 MB (69.97%) - LeetHub

--- 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
+++ 1027-longest-arithmetic-subsequence/1027-longest-arithmetic-subsequence.cpp
@@ -5,10 +5,11 @@ public:
         if(n<=2){return n;}
         int ans=1;
         vector<vector<int>> dp(n+1,vector<int>(1001,1));
+        // dp[i][diff] equals to the length of arithmetic sequence at i with difference diff
         for(int i=1;i<n;i++){
             for(int j=0;j<i;j++){
                 int dif=a[i]-a[j]+500;
-                dp[i][dif]=max(dp[i][dif],dp[j][dif]+1);
+                dp[i][dif]=max(2,dp[j][dif]+1);
                 ans=max(ans,dp[i][dif]);
             }
         }
