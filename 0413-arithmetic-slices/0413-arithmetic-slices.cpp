4aebe5bef27f45233be97b6e20bfbf53ba9b4a26
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-06-02 11:11:04 +0530

    Time: 6 ms (25.35%), Space: 7.3 MB (79.79%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.cpp
+++ 0413-arithmetic-slices/0413-arithmetic-slices.cpp
@@ -3,24 +3,12 @@ public:
     int numberOfArithmeticSlices(vector<int>& a) {
         int n=a.size();
         if(n==1){return 0;}
-        int dif=a[1]-a[0],cur=2;
+        int dif=a[1]-a[0],cur=0;
         int ans=0;
-        for(int i=2;i<=n;i++){
-            if(i==n){
-                if(cur>=3){
-                   ans+=(cur*(cur-2));
-                   ans-=(((cur*(cur-1))/2)-1);
-                }
-                break;
-            }
+        for(int i=2;i<n;i++){
             if(a[i]-a[i-1]==dif){cur++;}
-            else{
-                if(cur>=3){
-                   ans+=(cur*(cur-2));
-                   ans-=(((cur*(cur-1))/2)-1);
-                }
-                cur=2;dif=a[i]-a[i-1];
-            }
+            else{cur=0;dif=a[i]-a[i-1];}
+            ans+=cur;
         }
         return ans;
     }
