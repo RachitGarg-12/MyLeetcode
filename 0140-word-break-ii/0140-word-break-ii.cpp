ad6c1a9ccf918e0cf3a584f1e7f09a6d10bb61f2
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-06-02 10:41:54 +0530

    Time: 5 ms (28.12%), Space: 7.2 MB (55.28%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.cpp
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -3,17 +3,15 @@ public:
     int n;
     vector<string> ans;
     void f(int i,string &s,unordered_map<string,bool> &dict,string cur){
+        if(i==n){
+            cur.pop_back();
+            ans.push_back(cur);
+        }
         string pre="";
         for(int it=i;it<n;it++){
            pre+=s[it];
            if(dict.find(pre)!=dict.end()){
-               if(it==n-1){
-                   if(cur.size()==0){ans.push_back(pre);}
-                   else{ans.push_back(cur+' '+pre);}
-                   return;
-               }
-               if(cur.size()==0){f(it+1,s,dict,pre);}
-               else{f(it+1,s,dict,cur+' '+pre);}
+               f(it+1,s,dict,cur+pre+" ");
            }            
         }
     }
