adabdcf9252f71a20c3174ad0804e6e91659f3fe
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-06-04 16:10:38 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (39.62%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
-    int rec(int i1,int i2,int i3,string s1, string s2, string s3,vector<vector<vector<int>>>&dp){
-        if(i1==s1.size()&&i2==s2.size()&&i3==s3.size()){
-            return 1;
+    bool func(string &s1, string &s2, string &s3, int i1, int i3, vector<vector<int>>&dp){
+        if(i3 >= s3.length()){
+            return true;
         }
-        if(i3==s3.size())return 0;
-        if(dp[i1][i2][i3]!=-1)return dp[i1][i2][i3];
-        int a=0,b=0;
-        if(i1<s1.size() && s1[i1]==s3[i3]){
-            a=rec(i1+1,i2,i3+1,s1,s2,s3,dp);
+        
+        if(dp[i3][i1] != -1) return (bool)dp[i3][i1];
+        
+        int i2 = i3 - i1;
+        bool p1 = false, p2 = false;
+        
+        if(i1 < s1.length() && s1[i1] == s3[i3]){
+            p1 = func(s1, s2, s3, i1 + 1, i3 + 1, dp);
         }
-        if(i2<s2.size() && s2[i2]==s3[i3]){
-            b=rec(i1,i2+1,i3+1,s1,s2,s3,dp);
+        
+        if(i2 < s2.length() && s2[i2] == s3[i3]){
+            p2 = func(s1, s2, s3, i1, i3 + 1, dp);
         }
-        return dp[i1][i2][i3]=a||b;
+        
+        dp[i3][i1] = (int)(p1 || p2);
+        
+        return p1 || p2;
     }
     bool isInterleave(string s1, string s2, string s3) {
-        int n1=s1.size();
-        int n2=s2.size();
-        int n3=s3.size();
-        if(n1+n2!=n3){return false;}
-        vector<vector<vector<int>>>dp(n1+1,vector<vector<int>>(n2+1,vector<int>(n3+1,-1)));
-        return rec(0,0,0,s1,s2,s3,dp);
+        if(s1.length() + s2.length() != s3.length()) return false;
+        vector<vector<int>>dp(s3.length() + 1, vector<int>(s1.length() + 1, -1));
+        return func(s1, s2, s3, 0, 0, dp);
     }
 };
