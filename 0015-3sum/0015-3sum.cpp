058e65429ee8e1fac5699e43acee0f8d6521dd66
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-06-07 16:35:47 +0530

    Time: 116 ms (95.70%), Space: 24.1 MB (73.79%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -3,18 +3,21 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         int n=nums.size();
         sort(nums.begin(),nums.end());
-        set<vector<int>> ans;
+        vector<vector<int>> ans;
         for(int i=0;i<n-2;i++){
+            if(nums[i]>0)continue;
+            if(i>0 && nums[i]==nums[i-1]){continue;}
             int j=i+1,k=n-1;
             while(j<k){
                 int sum=nums[i]+nums[j]+nums[k];
-                if(sum==0){ans.insert({nums[i],nums[j],nums[k]});j++;k--;}
+                if(sum==0){ans.push_back({nums[i],nums[j],nums[k]});
+                  while(j<k && nums[j]==nums[j+1]){j++;}  
+                  while(j<k && nums[k]==nums[k-1]){k--;}
+                           j++;k--;}
                 else if(sum>0){k--;}
                 else{j++;}
             }
         }
-        vector<vector<int>> res;
-        for(auto i:ans){res.push_back(i);}
-        return res;
+        return ans;
     }
 };
