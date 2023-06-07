ee492b41e04d4881976581dde506cabff64581b9
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-06-07 16:08:23 +0530

    Time: 3 ms (96.42%), Space: 11.7 MB (70.95%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.cpp
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -13,22 +13,18 @@ public:
     ListNode* rotateRight(ListNode* head, int k) {
        if(k==0 || head==NULL){return head;}
        ListNode *temp=head;
-       int n=0;
-       while(temp){
+       int n=1;
+       while(temp->next){
             temp=temp->next;n++;
        }
        k%=n;
        if(k==0){return head;}
+       temp->next=head;
        temp=head;
        int dis=n-k-1;
        while(dis--){temp=temp->next;}
        ListNode *nhead=temp->next;
        temp->next=NULL;
-       ListNode *cur=nhead;
-       while(cur){
-            if(cur->next==NULL){cur->next=head;break;}
-            cur=cur->next;
-       }
        return nhead;
     }
 };
