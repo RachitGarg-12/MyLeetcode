85f400801d1a4b3f4ea8836783dff91b9fb9b7cb
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-06-08 16:46:44 +0530

    Time: 204 ms (39.71%), Space: 62.3 MB (43.31%) - LeetHub

--- 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
+++ 1235-maximum-profit-in-job-scheduling/1235-maximum-profit-in-job-scheduling.cpp
@@ -1,23 +1,32 @@
 class Solution {
 public:
-    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
-        // step 1: generate the sorting index such that endTime[index[i]] <= endTime[index[i+1]] for 0 <= i < n-1
-        auto comp = [&endTime](const int i1, const int i2) { return endTime[i1] < endTime[i2]; };
-        int n = endTime.size();
-        vector<int> index(n);
-        iota(index.begin(), index.end(), 0);
-        sort(index.begin(), index.end(), comp);
-        
-        // step 2: in order to perform binary search, we also need the sorting endTime array
-        vector<int> endSorted(endTime.begin(), endTime.end());
-        sort(endSorted.begin(), endSorted.end());
-        
-        // step 3: calculate dp table
-        vector<int> dp(n + 1);
-        for (int i = 1; i <= n; i++) {
-            int j = upper_bound(endSorted.begin(), endSorted.end(), startTime[index[i-1]]) - endSorted.begin();
-            dp[i] = max(dp[i-1], profit[index[i-1]] + dp[j]);
+    int n;
+    int getnext(int i,vector<vector<int>> &a){   //find next ind whose st > en of cur
+        int l=i+1,r=n-1,ans=n;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(a[i][1]<=a[mid][0]){
+                ans=mid;r=mid-1;
+            }else{l=mid+1;}
         }
-        return dp[n];
+        return ans;
+    }
+    int f(int i,vector<vector<int>> &a,vector<int> &dp){
+        if(i>=n){return 0;}
+        if(dp[i]!=-1){return dp[i];}
+        int nt=f(i+1,a,dp);  //not take cur
+        int in=getnext(i,a);
+        int take=a[i][2]+f(in,a,dp); //take cur , and go to next element whose start greater than en of cur
+        return dp[i]=max(nt,take);
+    }
+    int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
+        n=startTime.size();
+        vector<vector<int>> a;
+        for(int i=0;i<n;i++){
+            a.push_back({startTime[i],endTime[i],profit[i]});
+        }
+        sort(a.begin(),a.end());
+        vector<int> dp(n,-1);
+        return f(0,a,dp);
     }
 };
