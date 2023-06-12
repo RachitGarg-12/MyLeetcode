610675c812312c30c4c78389149e46366d7f9cdf
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-06-12 22:40:33 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (57.22%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.cpp
+++ 0228-summary-ranges/0228-summary-ranges.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    vector<string> summaryRanges(vector<int>& a) {
-        int n=a.size();
-        if(n==0){return {};}
-        if(n==1){return {to_string(a[0])};}
-        vector<string> res;
-        string ans="";
-        ans+=to_string(a[0]);
-        bool b=true;
-        for(int i=1;i<n;i++){
-            if(a[i]==a[i-1]+1){
-                if(ans.back()=='>'){continue;}
-                ans+="->";
-            }
-            else{
-                if(ans.back()=='>'){ans+=to_string(a[i-1]);}
-                res.push_back(ans);
-                ans=to_string(a[i]);
-                if(i==n-1){res.push_back(ans);}
-            }
-        }
-        if(ans.back()=='>'){ans+=to_string(a[n-1]);res.push_back(ans);}
-        return res;
+    vector<string> summaryRanges(vector<int>& nums) {
+       vector<string> Res;
+        if(nums.size() < 1){return Res;}
+        for(int i=0;i<nums.size();){
+            int low=i;
+            int high=i;
+            while(high+1 < nums.size() && nums[high+1] == nums[high]+1)
+                high++;
+            
+            if(high > low)
+                Res.push_back(to_string(nums[low]) + "->" + to_string(nums[high]));
+            
+            else
+                Res.push_back(to_string(nums[high]));
+            
+            i=high+1;
+            
+        }  
+        
+        return Res;
     }
 };
