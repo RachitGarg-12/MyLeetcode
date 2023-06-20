b9e4cc9d420007a8037e7b130d75e80a0d278c3f
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-06-20 17:03:41 +0530

    Time: 29 ms (89.42%), Space: 10.1 MB (79.43%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.cpp
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -1,38 +1,54 @@
 class Solution {
 public:
-    bool similar(string&a,string&b){
-        int dif=0,l=a.size();
-        for(int i=0;i<l;i++){
-            if(a[i]!=b[i]){dif++;}
-        }
-        return dif==0 || dif==2;
+    int find_par(int i,vector<int> &parent){
+        if(parent[i]==i)
+            return i;
+        return parent[i]=find_par(parent[i],parent);//path compression
     }
-    void dfs(int i,vector<int>&vis,vector<vector<int>>&graph){
-        vis[i]=1;
-        for(auto it:graph[i]){
-            if(!vis[it]){
-                dfs(it,vis,graph);
-            }
+    void union_(int i,int j,vector<int> &parent,vector<int> &rank)
+    {
+        i=find_par(i,parent);
+        j=find_par(j,parent);
+        if(rank[i]==rank[j])
+        {
+            parent[i]=j;
+            rank[j]++;
         }
+        else if(rank[i]>rank[j])
+            parent[j]=i;
+        else
+            parent[i]=j;
     }
-    int numSimilarGroups(vector<string>& strs) {
+
+    bool is_match(string &a,string &b)
+    {
+        int cnt=0;
+        for(int i=0;i<a.length();i++)
+        {
+            if(a[i]!=b[i])
+                cnt++;
+            if(cnt>2)
+                return false;
+        }
+        return true;
+    }
+    int numSimilarGroups(vector<string>& strs) 
+    {
         int n=strs.size();
-        vector<vector<int>> graph(n);
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(similar(strs[i],strs[j])){
-                    graph[i].push_back(j);
-                    graph[j].push_back(i);
-                }
-            }
-        }
-        vector<int> vis(n,0);
+        vector<int> parent(n),rank(n,0);
+        for(int i=0;i<n;i++)
+            parent[i]=i;
+
+        for(int i=0;i<n;i++)
+            for(int j=i+1;j<n;j++)
+                if(is_match(strs[i],strs[j]))
+                    union_(i,j,parent,rank);
+    
+        //to find number of groups just find number of parents (parent[i]==i)
         int ans=0;
-        for(int i=0;i<n;i++){
-            if(!vis[i]){
-                ans++;dfs(i,vis,graph);
-            }
-        }
+        for(int i=0;i<n;i++)
+            if(parent[i]==i)
+                ans++;
         return ans;
     }
 };
