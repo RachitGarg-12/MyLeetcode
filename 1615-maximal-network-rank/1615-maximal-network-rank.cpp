5b356cdc6a38e29f320c3308f0a6f27e71fbbcd2
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-06-20 12:42:11 +0530

    Time: 150 ms (23.44%), Space: 50.9 MB (8.74%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<int> indeg(n,0);
-        unordered_map<int,unordered_set<int>> m;
+        vector<unordered_set<int>> m(n);
         for(auto i:roads){
-            indeg[i[0]]++;
-            indeg[i[1]]++;
-            if(i[0]<i[1]){m[i[0]].insert(i[1]);}
-            else{m[i[1]].insert(i[0]);}
+            m[i[0]].insert(i[1]);
+            m[i[1]].insert(i[0]);
         }
         int ans=0;
         for(int i=0;i<n-1;i++){
             unordered_set<int> temp=m[i];
+            int p=temp.size();
             for(int j=i+1;j<n;j++){
-                if(temp.find(j)!=temp.end()){
-                    ans=max(ans,indeg[i]+indeg[j]-1);
+                int q=m[j].size();
+                if(temp.count(j)>0){
+                    ans=max(ans,p+q-1);
                 }
-                else{ans=max(ans,indeg[i]+indeg[j]);}
+                else{ans=max(ans,p+q);}
             }
         }
         return ans;
