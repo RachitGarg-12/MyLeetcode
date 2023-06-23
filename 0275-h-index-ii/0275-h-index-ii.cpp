8da7c351a1b3f40c0e3b9a84b645a90f5dbd6221
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-06-23 12:50:53 +0530

    Time: 19 ms (71.26%), Space: 18.7 MB (24.05%) - LeetHub

--- 0275-h-index-ii/0275-h-index-ii.cpp
+++ 0275-h-index-ii/0275-h-index-ii.cpp
@@ -2,16 +2,20 @@ class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n=citations.size();
-        int l=0,r=n-1,ans=-1;
+        int l=0,r=n-1,ans=0;
         while(l<=r){
             int m=l+(r-l)/2;
             int left=n-m;
-            if(left>=citations[m]){
-                ans=max(ans,citations[m]);l=m+1;
-            }else{
-                ans=max(ans,left);r=m-1;
+            if(left==citations[m]){
+                return left;
+            }
+            else if(left>citations[m]){
+                l=m+1;
+            }
+            else{
+                r=m-1;
             }
         }
-        return ans;
+        return n-l;
     }
 };
