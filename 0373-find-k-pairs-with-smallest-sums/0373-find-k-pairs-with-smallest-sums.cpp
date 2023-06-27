acd052535292d10f25c02f70eeb67d6a75d3300b
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-06-27 18:52:46 +0530

    Time: 386 ms (40.22%), Space: 110.1 MB (61.74%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -4,34 +4,24 @@ public:
           priority_queue<pair<int,pair<int,int>>> pq;
           int n = nums1.size();
           int m = nums2.size();
-
           for(int i=0;i<n;i++){
               for(int j=0; j<m ;j++){
-
                   int sum = nums1[i] + nums2[j];
-
                   if(pq.size() < k)
                     pq.push({sum,{nums1[i],nums2[j]}});
-
                    else if(sum < pq.top().first){
-
                         pq.pop();
                         pq.push({sum,{nums1[i],nums2[j]}});
                    }
-
-                    else
+                  else
                      break; 
               }
           }
-
           vector<vector<int>> ans;
-
            while(!pq.empty()){
-
                ans.push_back({pq.top().second.first,pq.top().second.second});
                pq.pop();
            }
-        
         return ans;        
     }
 };
