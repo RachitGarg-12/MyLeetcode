d0fb6c74c3f30c16f1ff3e359e2bd6379b2a29c3
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-07-01 10:23:16 +0530

    Time: 1811 ms (35.24%), Space: 6.8 MB (98.89%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.cpp
@@ -10,7 +10,7 @@ public:
     }
     void fun(int idx,vector<int>&cookies,vector<int>&v,int &k)
     {
-        if(idx >= cookies.size())  //if all the cookies are distributed try to make the potential answer
+        if(idx >= cookies.size()) 
         {
             int temp=*max_element(v.begin(),v.end());
             if(temp<ans)
@@ -19,8 +19,6 @@ public:
             }
             return;
         }
-        //we have the choise of the curr bag to give to any one among k so will try giving it to everyone
-        //using backtracking we will find the best optimal ans
         for(int i=0;i<k;i++)
         {
             v[i]=v[i]+cookies[idx];
