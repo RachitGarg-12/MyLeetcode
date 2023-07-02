0c893c0c8070f58935846667b8d3f39c6c42001c
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-07-02 07:54:59 +0530

    Time: 928 ms (22.55%), Space: 182.9 MB (28.43%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
-    int ans = 0;
-
-    void helper(int start, vector<vector<int>>& requests, vector<int>& indegree, int n, int count) {
-        if (start == requests.size()) {
-            for (int i = 0; i < n; i++) {
-                if (indegree[i] != 0) {
-                    return;
+    int maximumRequests(int n, vector<vector<int>>& a) {
+        int N = (int)a.size();
+        int ans = 0;
+        for(int i = 0 ; i < (1 << N) ; i++) {
+            int setbits = 0;
+            vector<int>cnt(n,0);
+            for(int j = 0 ; j < N ; j++) {
+                if(((1 << j) & (i))) {
+                    ++setbits;
+                    cnt[a[j][0]]--;
+                    cnt[a[j][1]]++;
                 }
             }
-            ans = max(ans, count);
-            return;
+            bool ok = 1;
+            for(int j = 0 ; j < n ; j++) {
+                if(cnt[j] != 0) ok = 0;
+            }
+            if(ok) {
+                ans = max(ans,setbits);
+            }
         }
-
-        // Take 
-        indegree[requests[start][0]]--;
-        indegree[requests[start][1]]++;
-        helper(start + 1,requests, indegree, n, count + 1);
-
-        // Not-take
-        indegree[requests[start][0]]++;
-        indegree[requests[start][1]]--;
-        helper(start + 1,requests, indegree, n, count);
-    }
-    
-    int maximumRequests(int n, vector<vector<int>>& requests) {
-        vector<int> indegree(n, 0);
-        helper(0, requests, indegree, n, 0);
         return ans;
     }
 };
