b7e84b841b046acf5048429e6d4882cf593bcf57
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-07-07 02:46:41 +0530

    Time: 44 ms (51.97%), Space: 29.1 MB (5.34%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,27 +1,23 @@
- class Solution {
+class Solution {
 public:
-		int minSubArrayLen(int target, vector<int>& nums) {
-
-		int i = 0, j = 0;
-		int sum = 0;
-		int n = nums.size();
-
-		for(auto it:nums) sum += it;
-		if(sum < target) return 0;
-		sum = 0;
-		int ans = INT_MAX;
-
-		while(j < n){
-			sum += nums[j];
-			while(sum >= target){
-                int len=j-i+1;
-				ans = min(ans,len);
-				sum -= nums[i];
-				i++;
-			}
-			j++;
-		}
-
-		return ans;  
+    int minSubArrayLen(int target, vector<int>& a) {
+        int n=a.size();
+        vector<int> pre(n);
+        pre[0]=a[0];
+        for(int i=1;i<n;i++){
+            pre[i]=a[i]+pre[i-1];
         }
+        if(pre[n-1]<target){return 0;}
+        int i=-1,cur=0,ans=n;
+        for(int j=0;j<n;j++){
+            cur=pre[j]-((i>=0)?pre[i]:0);
+            // cout<<i<<" "<<j<<" "<<cur<<endl;
+            while(i<j && cur>=target){
+                ans=min(ans,j-i);
+                i++;
+                cur=pre[j]-((i>=0)?pre[i]:0);
+            }
+        }
+        return ans;
+    }
 };
