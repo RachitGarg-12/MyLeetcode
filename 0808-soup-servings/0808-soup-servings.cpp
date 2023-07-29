bca80aa642fa13ff08a257847e2f0f1fd9d62a08
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-07-29 20:40:19 +0530

    Time: 3 ms (86.69%), Space: 9.9 MB (69.20%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.cpp
+++ 0808-soup-servings/0808-soup-servings.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> quan={{100,0},{75,25},{50,50},{25,75}};
-    double prob(int a,int b,map<pair<int,int>,double> &dp){
+    map<pair<int,int>,double> dp;
+    double prob(int a,int b){
         if(a<=0 && b<=0){return 0.5;}
         if(a<=0){return 1;}
         if(b<=0){return 0;}
         if(dp.find({a,b})!=dp.end()){return dp[{a,b}];}
         double ans=0;
         for(int i=0;i<4;i++){
-            ans+=0.25*prob(a-quan[i][0],b-quan[i][1],dp);
+            ans+=0.25*prob(a-quan[i][0],b-quan[i][1]);
         }
         return dp[{a,b}]=ans;
     }
     double soupServings(int n) {
         if(n>5000){return 1;}
-        map<pair<int,int>,double> dp;
-        return prob(n,n,dp);
+        return prob(n,n);
     }
 };
