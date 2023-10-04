01a57fa8f4c7ba6cf6f4e6166f03c80f8f86e605
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-10-04 23:49:31 +0530

    Added solution - LeetHub

---
+++ Roman Number to Integer - GFG/roman-number-to-integer.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution {
+  public:
+    int romanToDecimal(string &str) {
+        int ans=0,n=str.size();
+        unordered_map<char,int> m;
+        m['I']=1;
+        m['V']=5;
+        m['X']=10;
+        m['L']=50;
+        m['C']=100;
+        m['D']=500;
+        m['M']=1000;
+        for(int i=0;i<n-1;i++){
+            if(m[str[i+1]]>m[str[i]]){ans-=m[str[i]];}
+            else{ans+=m[str[i]];}
+        }
+        ans+=m[str[n-1]];
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.romanToDecimal(s) << endl;
+    }
+}
+// } Driver Code Ends
