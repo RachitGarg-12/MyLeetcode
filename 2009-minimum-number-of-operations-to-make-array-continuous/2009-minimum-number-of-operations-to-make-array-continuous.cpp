f1ffc9442c103a7454c060eb9eb0f3434003756c
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-10-10 10:27:29 +0530

    Time: 181 ms (64.44%), Space: 64.7 MB (69.67%) - LeetHub

--- 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -6,7 +6,7 @@ public:
         sort(nums.begin(), nums.end());
         auto it=unique(nums.begin(), nums.end());
         nums.erase(it, nums.end());//erase the repetive elements
-        int m=nums.size(), k=n-m;
+        int m=nums.size();
         for(int i=0; i<m; i++){
             int l=nums[i], r=l+n-1;
             int j=upper_bound(nums.begin()+i, nums.end(), r)-nums.begin();
