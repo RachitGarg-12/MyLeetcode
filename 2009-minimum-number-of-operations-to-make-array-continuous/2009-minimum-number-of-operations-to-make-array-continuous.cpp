d18cc8bd9ec05547a2032e8ae457eedca8e463d6
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-10-10 10:27:04 +0530

    Time: 199 ms (55.02%), Space: 64.6 MB (71.55%) - LeetHub

---
+++ 2009-minimum-number-of-operations-to-make-array-continuous/2009-minimum-number-of-operations-to-make-array-continuous.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n=nums.size();
+        int ans=n-1;
+        sort(nums.begin(), nums.end());
+        auto it=unique(nums.begin(), nums.end());
+        nums.erase(it, nums.end());//erase the repetive elements
+        int m=nums.size(), k=n-m;
+        for(int i=0; i<m; i++){
+            int l=nums[i], r=l+n-1;
+            int j=upper_bound(nums.begin()+i, nums.end(), r)-nums.begin();
+            ans=min(ans, n-j+i);
+        }
+        
+        return ans;
+    }
+};
