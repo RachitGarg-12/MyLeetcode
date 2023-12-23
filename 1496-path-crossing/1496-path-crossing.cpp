26043b94cc7804872a9661426062718993205929
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2023-12-23 08:54:26 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (64.64%) - LeetHub

--- 1496-path-crossing/1496-path-crossing.cpp
+++ 1496-path-crossing/1496-path-crossing.cpp
@@ -3,15 +3,15 @@ public:
     bool isPathCrossing(string path) {
         int x=0,y=0;
         int n=path.size();
-        map<pair<int,int>,bool> mp;
-        mp[{0,0}]=true;
+        set<pair<int,int>> s;
+        s.insert({0,0});
         for(int i=0;i<n;i++){
             if(path[i]=='N'){x++;}
             if(path[i]=='S'){x--;}
             if(path[i]=='E'){y++;}
             if(path[i]=='W'){y--;}
-            if(mp.find({x,y})!=mp.end()){return true;}
-            mp[{x,y}]=true;
+            if(s.find({x,y})!=s.end()){return true;}
+            s.insert({x,y});
         }
         return false;
     }
