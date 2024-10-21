a11cb46d1f2a60d09c5597193c40f1e02de63f36
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2024-10-21 18:49:55 +0530

    Time: 1097 ms (8.77%), Space: 423.6 MB (8.19%) - LeetHub

--- 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1593-split-a-string-into-the-max-number-of-unique-substrings/1593-split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    int count(int i,string &s,map<string,bool> mp){
+    int count(int i,string &s,set<string> mp){
         if(i==s.size())return 0;
         string cur="";
         int ans=0;
         for(int j=i;j<s.size();j++){
             cur+=s[j];
             if(mp.find(cur)==mp.end()){
-                mp[cur]=true;
+                mp.insert(cur);
                 ans=max(ans,1+count(j+1,s,mp));
                 mp.erase(cur);
             }
@@ -15,7 +15,7 @@ public:
         return ans;
     }
     int maxUniqueSplit(string s) {
-        map<string,bool> mp;
+        set<string> mp;
         return count(0,s,mp);
     }
 };
