26235d3c99f37a00224f932494910648b5b62c25
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2024-10-23 23:37:05 +0530

    Time: 613 ms (90.87%), Space: 357.7 MB (15.59%) - LeetHub

---
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        root->val = 0;
+        dfs(std::vector<TreeNode*>{root});
+        return root;
+    }
+
+private:
+    void dfs(std::vector<TreeNode*> arr) {
+        if (arr.empty()) return;
+
+        int sum = 0;
+        for (auto node : arr) {
+            if (!node) continue;
+            if (node->left) sum += node->left->val;
+            if (node->right) sum += node->right->val;
+        }
+
+        std::vector<TreeNode*> childArr;
+        for (auto node : arr) {
+            int curSum = 0;
+            if (node->left) curSum += node->left->val;
+            if (node->right) curSum += node->right->val;
+
+            if (node->left) {
+                node->left->val = sum - curSum;
+                childArr.push_back(node->left);
+            }
+            if (node->right) {
+                node->right->val = sum - curSum;
+                childArr.push_back(node->right);
+            }
+        }
+
+        dfs(childArr);
+    }
+};
