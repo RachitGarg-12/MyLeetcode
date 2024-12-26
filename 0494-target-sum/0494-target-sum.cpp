003cdf0fa5aa696c1b440af2cb15bd1747c2b81a
Rachit Garg <87380094+RachitGarg-12@users.noreply.github.com>
2024-12-27 00:29:16 +0530

    Time: 11 ms (64.63%), Space: 12.4 MB (61.72%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
+    int dp[21][10001];
     int ways(int i,vector<int>& nums, int target){
         if(i==nums.size()){
             if(target==0)return 1;
             return 0;
         }
+        if(dp[i][1000+target]!=-1){
+            return dp[i][1000+target];
+        }
         int ans=0;
         ans+=ways(i+1,nums,target+nums[i]);
         ans+=ways(i+1,nums,target-nums[i]);
-        return ans;
+        return dp[i][1000+target]=ans;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
+        memset(dp,-1,sizeof(dp));
         return ways(0,nums,target);
     }
 };
